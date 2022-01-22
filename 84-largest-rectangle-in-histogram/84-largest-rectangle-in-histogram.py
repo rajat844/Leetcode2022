91f9d5cdf5ed167393da6562784abe2a19712e3c
Rajat Kumar <54264843+rajat844@users.noreply.github.com>
2022-01-22 10:58:17 +0530

    Time: 1425 ms (12.61%), Space: 27.5 MB (77.04%) - LeetHub

--- 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.py
+++ 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.py
@@ -1,36 +1,17 @@
 class Solution:
     def largestRectangleArea(self, heights: List[int]) -> int:
-        stack,left,right = [],[],[]
-        for i in range(len(heights)):
-            while len(stack) > 0 and heights[stack[-1]] >= heights[i]:
+        stack = []
+        maxA = 0
+        for i in range(0,len(heights)+1,1):
+            while len(stack) > 0 and (i == len(heights) or heights[stack[-1]] >= heights[i]):
+                hi = heights[stack[-1]]
                 stack.pop()
-            if len(stack) == 0 :
-                left.append(0)
-            else :
-                left.append(stack[-1] + 1 )
-
+                wi = 0
+                if len(stack) == 0:
+                    wi = i
+                else :
+                    wi = i - stack[-1] -1
+                maxA = max(maxA,wi * hi)  
             stack.append(i)
-        
-        stack.clear()
-        
-        for i in range(len(heights)-1,-1,-1):
-            while len(stack) > 0 and heights[stack[-1]] >= heights[i]:
-                stack.pop()
-            
-            if len(stack) == 0:
-                right.append(len(heights)-1)
-            else :
-                right.append(stack[-1] - 1)
-
-            stack.append(i)
-
-        right.reverse()
-        
-        ans = []
-        
-        for i in range(len(heights)):
-            ans.append(heights[i] * (right[i] - left[i] + 1))
-        
-        return max(ans)
-
+        return maxA
         

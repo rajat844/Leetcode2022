751ce69c545f39df63c6caec82fc95ad1d3f7434
Rajat Kumar <54264843+rajat844@users.noreply.github.com>
2022-02-03 13:02:23 +0530

    Time: 76 ms (75.77%), Space: 20.5 MB (48.38%) - LeetHub

---
+++ 173-binary-search-tree-iterator/173-binary-search-tree-iterator.py
@@ -0,0 +1,33 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class BSTIterator:
+
+    def __init__(self, root: Optional[TreeNode]):
+        self.st = []
+        self.pushAll(root)
+    
+    def pushAll(self,root: Optional[TreeNode]):
+        while root :
+            self.st.append(root)
+            root = root.left
+
+    def next(self) -> int:
+        temp = self.st.pop()
+        self.pushAll(temp.right)
+        return temp.val
+        
+
+    def hasNext(self) -> bool:
+        return len(self.st) > 0
+            
+        
+
+
+# Your BSTIterator object will be instantiated and called as such:
+# obj = BSTIterator(root)
+# param_1 = obj.next()
+# param_2 = obj.hasNext()

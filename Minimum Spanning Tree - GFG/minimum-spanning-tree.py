e0a35fb7ef5ccb5e4626ef150b0b1e2c92fbda57
Rajat Kumar <54264843+rajat844@users.noreply.github.com>
2022-02-14 12:16:44 +0530

    Added solution - LeetHub

--- Minimum Spanning Tree - GFG/minimum-spanning-tree.py
+++ Minimum Spanning Tree - GFG/minimum-spanning-tree.py
@@ -1,51 +1,48 @@
 #User function Template for python3
+
 class Solution:
+    
+    #Function to find sum of weights of edges of the Minimum Spanning Tree.
     def spanningTree(self, V, adj):
-        # sorting the adj nodes according to cost/ weight 
-        Adj = []
-        for node in range(V):
-            for dest, cost in adj[node]:
-                Adj.append([node, dest, cost])
-        Adj.sort(key = lambda x : x[2])  
-        
-        # creating parent and rank arr
-        parent_arr = []
-        rank_arr = [0]*V
-        for node in range(V):
-            parent_arr.append(node)
-            
-        # Disjoint set data structure 
+        #code here
         def findParent(node):
-            if node == parent_arr[node]:
+            if node == parent[node]:
                 return node
-            parent_arr[node] = findParent(parent_arr[node])
-            return parent_arr[node]
+            parent[node] = findParent(parent[node])
+            return parent[node]
             
         def union(node1, node2):
-            node1 = findParent(node1)
+            node1 = findParent(node1) 
             node2 = findParent(node2)
-            
-            if rank_arr[node1] < rank_arr[node2]:
-                parent_arr[node1] = node2
-            elif rank_arr[node1] > rank_arr[node2]:
-                parent_arr[node2] = node1
-            else:
-                parent_arr[node2] = node1
-                rank_arr[node1]+=1
-        
-
-        min_cost = 0
-        res_tree = [] # this is optional this will store the tree itself
+            if rank[node1] == rank[node2]:
+                rank[node1] += 1
+                parent[node1] = node2
+            elif rank[node2] > rank[node1]:
+                parent[node1] = node2
+            elif rank[node1] > rank[node2]:
+                parent[node2] = node1
+                
     
-        for arr in Adj:
-            src = arr[0]
-            dest = arr[1]
-            cost = arr[2]
-            if findParent(src)!=findParent(dest):
-                min_cost += cost
-                res_tree.append((src, dest))
-                union(src, dest)
-        return min_cost
+        parent = []
+        for node in range(V):
+            parent.append(node)
+        rank = [0] * V
+        st = []
+        for i in range(len(adj)):
+            for j in adj[i]:
+                st.append((j[1],j[0],i))
+        
+        st.sort(key = lambda x:x[0])
+        ans = 0
+        resTree = []
+        for arr in st:
+            edge, node1, node2 = arr[0],arr[1],arr[2]
+            if findParent(node1) != findParent(node2) :
+                union(node1,node2)
+                resTree.append((node1,node2))
+                ans += edge
+        
+        return ans
             
         #code here
 

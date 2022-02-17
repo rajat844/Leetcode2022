131cacea9ee5be42f6af80546f49680b3076e2ad
Rajat Kumar <54264843+rajat844@users.noreply.github.com>
2022-02-17 19:53:53 +0530

    Added solution - LeetHub

--- Subset Sum Problem - GFG/subset-sum-problem.py
+++ Subset Sum Problem - GFG/subset-sum-problem.py
@@ -1,23 +1,49 @@
 #User function Template for python3
 
+# class Solution:
+#     def isSubsetSum (self, N, arr, sum):
+#         # code here 
+#         dp = [[False for i in range(sum+1)]for j in range(N+1)]
+
+#         for i in range(N+1):
+#             dp[i][0] = True
+            
+        
+#         for i in range(1,N+1):
+#             for j in range(1,sum+1):
+#                 if j < arr[i-1]:
+#                     dp[i][j] = dp[i-1][j]
+                
+#                 else :
+#                     dp[i][j] = dp[i-1][j] or dp[i-1][j-arr[i-1]]
+                    
+#         return dp[N][sum]
+
+
 class Solution:
     def isSubsetSum (self, N, arr, sum):
         # code here 
-        dp = [[False for i in range(sum+1)]for j in range(N+1)]
-
-        for i in range(N+1):
-            dp[i][0] = True
-            
+        dp = [[None for i in range(sum+1)]for j in range(N+1)]
         
-        for i in range(1,N+1):
-            for j in range(1,sum+1):
-                if j < arr[i-1]:
-                    dp[i][j] = dp[i-1][j]
+        def helper(N,sum):     
+            if N == 0:
+                return sum == 0
+
+            if sum == 0:
+                return True
                 
-                else :
-                    dp[i][j] = dp[i-1][j] or dp[i-1][j-arr[i-1]]
-                    
-        return dp[N][sum]
+            if dp[N][sum] != None:
+                return dp[N][sum]
+            
+            if arr[N-1] > sum:
+                dp[N][sum] = helper(N-1,sum)
+                return dp[N][sum]
+                
+            else :
+                dp[N][sum] = helper(N-1,sum) or helper(N-1,sum-arr[N-1])
+                return dp[N][sum]
+                
+        return helper(N,sum)
 
 #{ 
 #  Driver Code Starts

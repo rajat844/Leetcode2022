0abe60e7a438dd7d17c6e0c0c0554625ec94bce2
Rajat Kumar <54264843+rajat844@users.noreply.github.com>
2022-02-17 10:26:17 +0530

    Added solution - LeetHub

--- 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.py
+++ 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.py
@@ -6,22 +6,39 @@ class Solution:
     def knapSack(self,W, wt, val, n):
         dp = [[-1 for i in range(W+1)]for j in range(n+1)]
         
-        def helper(n,W):
-            if n == 0 or W == 0:
-                return 0
+        # def helper(n,W):
+        #     if n == 0 or W == 0:
+        #         return 0
                 
-            if dp[n][W] != -1:
-                return dp[n][W]
+        #     if dp[n][W] != -1:
+        #         return dp[n][W]
                 
-            if W < wt[n-1]:
-                dp[n][W] = helper(n-1,W)
-                return dp[n][W]
+        #     if W < wt[n-1]:
+        #         dp[n][W] = helper(n-1,W)
+        #         return dp[n][W]
                 
-            else :
-                dp[n][W] = max(helper(n-1,W) , val[n-1] + helper(n-1,W-wt[n-1]))
-                return dp[n][W]
+        #     else :
+        #         dp[n][W] = max(helper(n-1,W) , val[n-1] + helper(n-1,W-wt[n-1]))
+        #         return dp[n][W]
                 
-        return helper(n,W)
+        # return helper(n,W)
+        
+        for i in range(n+1):
+            for j in range(W+1):
+                if i == 0 or j == 0:
+                    dp[i][j] = 0
+                
+        for i in range(1,n+1):
+            for j in range(1,W+1):
+                if j < wt[i-1]:
+                    dp[i][j] = dp[i-1][j]
+                else :
+                    dp[i][j] = max(dp[i-1][j] , val[i-1] + dp[i-1][j - wt[i-1]])
+                    
+        return dp[n][W]
+        
+        
+        
         # code here
 
 #{ 

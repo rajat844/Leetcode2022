d80dd3cf47ec8e0620281051812cb85e091815a2
Rajat Kumar <54264843+rajat844@users.noreply.github.com>
2022-03-03 01:42:09 +0530

    Added solution - LeetHub

--- Rod Cutting - GFG/rod-cutting.py
+++ Rod Cutting - GFG/rod-cutting.py
@@ -6,22 +6,32 @@ class Solution:
         length = [i+1 for i in range(n)]
         st = [[-1 for i in range(n+1)]for j in range(n+1)]
         
-        def helper(n,l):
-            if n == 0 or l== 0:
-                st[n][l] = 0
+        # def helper(n,l):
+        #     if n == 0 or l== 0:
+        #         st[n][l] = 0
                 
-            if st[n][l] != -1:
-                return st[n][l]
-            else :
-                if l >= n :
-                    st[n][l] = max(helper(n-1,l),price[n-1] + helper(n,l-length[n-1]))
-                    return st[n][l]
+        #     if st[n][l] != -1:
+        #         return st[n][l]
+        #     else :
+        #         if l >= n :
+        #             st[n][l] = max(helper(n-1,l),price[n-1] + helper(n,l-length[n-1]))
+        #             return st[n][l]
+        #         else :
+        #             st[n][l] = helper(n-1,l)
+        #             return st[n][l]
+        
+        
+        # helper(n,n)
+        
+        for i in range(n+1):
+            for j in range(n+1):
+                if i == 0 or j == 0:
+                    st[i][j] = 0
+                elif (j >= length[i-1]):
+                    st[i][j] = max(st[i-1][j],price[i-1] + st[i][j-length[i-1]])
                 else :
-                    st[n][l] = helper(n-1,l)
-                    return st[n][l]
-        
-        
-        helper(n,n)
+                    st[i][j] = st[i-1][j]
+                
         return st[n][n]
 
 #{ 

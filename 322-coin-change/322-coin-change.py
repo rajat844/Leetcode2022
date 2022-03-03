a314068993d76df898baf15c362a7418687d0631
Rajat Kumar <54264843+rajat844@users.noreply.github.com>
2022-03-04 00:14:44 +0530

    Time: 2002 ms (49.56%), Space: 14.1 MB (79.17%) - LeetHub

---
+++ 322-coin-change/322-coin-change.py
@@ -0,0 +1,16 @@
+import math
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        dp = [math.inf for i in range(amount+1)]
+        dp[0] = 0
+
+        for i in range(1, amount+1):
+            for j in range(len(coins)):
+                if coins[j] <= amount:
+                    if dp[i-coins[j]] != math.inf and dp[i-coins[j]] + 1 < dp[i]:
+                        dp[i] = dp[i-coins[j]] + 1
+
+        if  dp[amount] == math.inf:
+            return -1
+        
+        return int(dp[amount])

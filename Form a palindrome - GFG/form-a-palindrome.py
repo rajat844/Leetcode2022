f05a4a51eca5c1a66fda21997f6da0c7a2399587
Rajat Kumar <54264843+rajat844@users.noreply.github.com>
2022-03-05 21:26:33 +0530

    Added solution - LeetHub

---
+++ Form a palindrome - GFG/form-a-palindrome.py
@@ -0,0 +1,39 @@
+#User function Template for python3
+
+class Solution:
+    def countMin(self, Str):
+        # code here
+        n = len(Str)
+        str2 = Str[::-1]
+        
+        dp = [[-1 for i in range(n+1)]for j in range(n+1)]
+        
+        for i in range(n+1):
+            for j in range(n+1):
+                if i == 0 or j == 0:
+                    dp[i][j] = 0
+                elif Str[i-1] == str2[j-1]:
+                    dp[i][j] = dp[i-1][j-1] + 1
+                else :
+                    dp[i][j] = max(dp[i-1][j],dp[i][j-1])
+                    
+        ans = n - dp[i][j]
+        return ans
+                
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+
+    t = int(input())
+
+    for _ in range(t):
+        Str = input()
+        
+
+        solObj = Solution()
+
+        print(solObj.countMin(Str))
+# } Driver Code Ends

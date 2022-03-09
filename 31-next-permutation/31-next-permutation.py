020e9a5717a6e82a5d22418e1d893b23b72f19b8
Rajat Kumar <54264843+rajat844@users.noreply.github.com>
2022-03-09 19:37:03 +0530

    Time: 44 ms (86.06%), Space: 14 MB (52.43%) - LeetHub

--- 31-next-permutation/31-next-permutation.py
+++ 31-next-permutation/31-next-permutation.py
@@ -22,7 +22,6 @@ class Solution:
                 j -= 1
             
             nums[i],nums[j] = nums[j],nums[i]
-            
-            nums[i+1:] = sorted(nums[i+1:])
+            nums[i+1:] = nums[i+1:][::-1]
             
         return nums

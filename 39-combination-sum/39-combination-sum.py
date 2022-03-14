98bb1afcb8a9aa893c0bd6b431b57f6b06d67375
Rajat Kumar <54264843+rajat844@users.noreply.github.com>
2022-03-14 15:37:45 +0530

    Time: 226 ms (11.50%), Space: 13.9 MB (68.46%) - LeetHub

--- 39-combination-sum/39-combination-sum.py
+++ 39-combination-sum/39-combination-sum.py
@@ -1,20 +1,24 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        
         ans = []
-        n = len(candidates)
+        candidates.sort()
         
-        def helper(index,s):
-            if index == n or sum(s) >= target:
+        def helper(i,s):
+            if i == len(candidates) or sum(s) >= target:
                 if sum(s) == target:
-                    temp = []
-                    for e in s:
-                        temp.append(e)
-                    ans.append(temp)
+                    ans.append(s[::])
                 return
-            s.append(candidates[index])
-            helper(index,s)
+            
+            s.append(candidates[i])
+            helper(i,s)
             s.pop()
-            helper(index+1,s)
-        
+            
+            while i+1 < len(candidates) and candidates[i] == candidates[i+1]:
+                i += 1
+            
+            helper(i+1,s)
+            
         helper(0,[])
         return ans
+        

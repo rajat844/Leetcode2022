fb1187407c68225e76760d19e4b718dfce52275b
Rajat Kumar <54264843+rajat844@users.noreply.github.com>
2022-03-14 15:13:45 +0530

    Time: 38 ms (87.91%), Space: 14.2 MB (64.41%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.py
+++ 90-subsets-ii/90-subsets-ii.py
@@ -2,23 +2,19 @@ class Solution:
     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
         nums.sort()
         ans = []
-        n = len(nums)
-
-        def helper(s, i):
-            if i == n:
-                if s not in ans:
-                    t = []
-                    for x in range(len(s)):
-                        t.append(s[x])
-                    ans.append(t)
-                return
-
-            for index in range(i, n):
-                s.append(nums[index])
-                helper(s,index+1)
-                s.pop()            
-                helper(s,index+1)
-        s = []
-        helper(s, 0)
-        return ans
         
+        def helper(index,s):
+            if index == len(nums):
+                ans.append(s[::])
+                return
+            s.append(nums[index])
+            helper(index + 1,s)
+            s.pop()
+            
+            while index + 1 <len(nums) and nums[index] == nums[index+1]:
+                index  += 1
+            helper(index+1,s)
+            
+                
+        helper(0,[])
+        return ans

b15ad23a4ef37b7fb709389e2a46aff848eda1bd
Rajat Kumar <54264843+rajat844@users.noreply.github.com>
2022-03-19 19:49:03 +0530

    Time: 40 ms (98.26%), Space: 14 MB (45.23%) - LeetHub

---
+++ 13-roman-to-integer/13-roman-to-integer.py
@@ -0,0 +1,13 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        mp = {'I':1, 'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
+        ans = 0
+        for i in range(len(s)):
+            if i != len(s) - 1 and mp[s[i+1]] > mp[s[i]]:
+                ans += -mp[s[i]]
+            else:
+                ans += mp[s[i]]
+                
+        return ans
+                       
+        

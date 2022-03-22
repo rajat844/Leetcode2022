443d299774c7f06f96c6a09686dc9b37d95c5c4b
Rajat Kumar <54264843+rajat844@users.noreply.github.com>
2022-03-22 07:16:51 +0530

    Time: 40 ms (83.83%), Space: 13.9 MB (92.87%) - LeetHub

--- 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.py
+++ 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.py
@@ -4,22 +4,20 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-import math
 class Solution:
+    import math
     def bstFromPreorder(self, preorder: List[int]) -> Optional[TreeNode]:
+        
         def helper(upperbound):
             nonlocal i
-            if i == n or preorder[i] >= upperbound:
+            if i == len(preorder) or preorder[i] > upperbound:
                 return None
-            
             root = TreeNode(preorder[i])
             i += 1
             root.left = helper(root.val)
             root.right = helper(upperbound)
             return root
-        upperbound = math.inf
+        
         i = 0
-        n = len(preorder)
-        return helper(upperbound)
-        
-        
+        return helper(math.inf)
+            

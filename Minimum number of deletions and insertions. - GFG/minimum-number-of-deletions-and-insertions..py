f8a1959e7148410731600192dc283f16b4b7bcae
Rajat Kumar <54264843+rajat844@users.noreply.github.com>
2022-03-23 23:24:52 +0530

    Added solution - LeetHub

--- Minimum number of deletions and insertions. - GFG/minimum-number-of-deletions-and-insertions..py
+++ Minimum number of deletions and insertions. - GFG/minimum-number-of-deletions-and-insertions..py
@@ -1,26 +1,28 @@
 #User function Template for python3
+import math
 class Solution:
-	def minOperations(self, s1, s2):
-		# code here
-		n = len(s1)
-		m = len(s2)
-		
-		dp  = [[-1 for j in range(m+1)]for i in range(n+1)]
-		
-		
-		for i in range(n+1):
-		    for j in range(m+1):
-		        if i == 0 or j == 0:
-		            dp[i][j] = 0
-		        
-		        elif s1[i-1]==s2[j-1]:
-		            dp[i][j] = dp[i-1][j-1]+1
-		        else:
-		            dp[i][j] = max(dp[i-1][j],dp[i][j-1])
-		            
-		ans = n - dp[n][m] + m - dp[n][m]
-		return ans
-		       
+    def minOperations(self, word1, word2) -> int:
+        n = len(word1)
+        m = len(word2)
+        
+        dp = [[math.inf for i in range(m+1)]for j in range(n+1)]
+        
+        for i in range(n+1):
+            dp[i][0] = i
+        
+        for j in range(m+1):
+            dp[0][j] = j 
+        
+        for i in range(1,n+1):
+            for j in range(1,m+1):
+                   
+                if word1[i-1] == word2[j-1] :
+                    dp[i][j] = dp[i-1][j-1]
+                
+                else :
+                    dp[i][j] = 1 + min(dp[i][j-1],dp[i-1][j])
+        
+        return dp[n][m]
 #{ 
 #  Driver Code Starts
 #Initial Template for Python 3

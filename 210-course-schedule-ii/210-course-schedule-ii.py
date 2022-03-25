03e8e38f1318535b22c1a6cf40f092280aa2ba5d
Rajat Kumar <54264843+rajat844@users.noreply.github.com>
2022-03-25 23:21:19 +0530

    Time: 187 ms (24.27%), Space: 17.3 MB (29.23%) - LeetHub

---
+++ 210-course-schedule-ii/210-course-schedule-ii.py
@@ -0,0 +1,33 @@
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        adj = defaultdict(list)
+
+        for i in range(len(prerequisites)):
+            adj[prerequisites[i][0]].append(prerequisites[i][1])
+
+        visited = [False for i in range(numCourses)]
+        dfsvisited = [False for i in range(numCourses)]
+
+        def helper(node, parent,lst):
+            visited[node] = True
+            dfsvisited[node] = True
+            for x in adj[node]:
+                if visited[x] == False:
+                    if helper(x, node,lst):
+                        return True
+                    
+                elif dfsvisited[x] == True:
+                    return True
+                
+            dfsvisited[node] = False
+            ans.append(node)
+            return False
+        
+        ans = []
+        for i in range(numCourses):
+            if visited[i] == False:
+                if helper(i, -1,ans) == True:
+                    return []
+                
+        return ans
+        

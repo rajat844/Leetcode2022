5c06fdd9d913e02ddd6b5c90134199212b91e5b3
Rajat Kumar <54264843+rajat844@users.noreply.github.com>
2022-03-25 23:32:17 +0530

    Time: 175 ms (31.26%), Space: 17.3 MB (24.07%) - LeetHub

--- 210-course-schedule-ii/210-course-schedule-ii.py
+++ 210-course-schedule-ii/210-course-schedule-ii.py
@@ -4,30 +4,30 @@ class Solution:
 
         for i in range(len(prerequisites)):
             adj[prerequisites[i][0]].append(prerequisites[i][1])
-
+            
         visited = [False for i in range(numCourses)]
         dfsvisited = [False for i in range(numCourses)]
 
-        def helper(node, parent,lst):
+        def helper(node): 
             visited[node] = True
             dfsvisited[node] = True
+            
             for x in adj[node]:
                 if visited[x] == False:
-                    if helper(x, node,lst):
+                    if helper(x) == True:
                         return True
-                    
                 elif dfsvisited[x] == True:
                     return True
-                
+            
             dfsvisited[node] = False
             ans.append(node)
             return False
         
+        
         ans = []
         for i in range(numCourses):
             if visited[i] == False:
-                if helper(i, -1,ans) == True:
+                if helper(i) == True:
                     return []
-                
-        return ans
         
+        return ans

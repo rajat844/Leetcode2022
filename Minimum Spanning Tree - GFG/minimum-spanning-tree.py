39c2a837336ff8833066367803028d11b16b5ea0
Rajat Kumar <54264843+rajat844@users.noreply.github.com>
2022-03-25 10:06:21 +0530

    Added solution - LeetHub

--- Minimum Spanning Tree - GFG/minimum-spanning-tree.py
+++ Minimum Spanning Tree - GFG/minimum-spanning-tree.py
@@ -4,47 +4,53 @@ class Solution:
     
     #Function to find sum of weights of edges of the Minimum Spanning Tree.
     def spanningTree(self, V, adj):
-        #code here
         def findParent(node):
-            if node == parent[node]:
+            if parent[node] == node:
                 return node
             parent[node] = findParent(parent[node])
             return parent[node]
-            
-        def union(node1, node2):
-            node1 = findParent(node1) 
+        
+        def union(node1 , node2):
+            node1 = findParent(node1)
             node2 = findParent(node2)
+            
             if rank[node1] == rank[node2]:
-                rank[node1] += 1
+                rank[node2] += 1
+                parent[node2] = node1
+            
+            if rank[node1] > rank[node2]:
                 parent[node1] = node2
-            elif rank[node2] > rank[node1]:
-                parent[node1] = node2
-            elif rank[node1] > rank[node2]:
+                
+            if rank[node2] > rank[node1]:
                 parent[node2] = node1
                 
-    
-        parent = []
-        for node in range(V):
-            parent.append(node)
-        rank = [0] * V
-        st = []
-        for i in range(len(adj)):
-            for j in adj[i]:
-                st.append((j[1],j[0],i))
-        
-        st.sort(key = lambda x:x[0])
-        ans = 0
-        resTree = []
-        for arr in st:
-            edge, node1, node2 = arr[0],arr[1],arr[2]
-            if findParent(node1) != findParent(node2) :
-                union(node1,node2)
-                resTree.append((node1,node2))
-                ans += edge
-        
-        return ans
             
         #code here
+        parent = []
+        rank = []
+        
+        for i in range(V):
+            parent.append(i)
+            rank.append(0)
+        
+        st = []
+        
+        for i in range(len(adj)):
+            for j in adj[i]:
+              st.append((i,j[0],j[1]))
+              
+        st.sort(key = lambda x: x[2])
+        
+        ans = 0
+        mst = []
+        for i in range(len(st)):
+            node1,node2,edge = st[i]
+            if findParent(node1) != findParent(node2):
+                union(node1,node2)
+                ans += edge
+                mst.append((node1,node2))
+                
+        return ans
 
 #{ 
 #  Driver Code Starts

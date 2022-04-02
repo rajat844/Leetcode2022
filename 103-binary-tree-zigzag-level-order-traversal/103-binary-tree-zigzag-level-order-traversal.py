48056d97e897bb85df5ca5ccbead682d16886e66
Rajat Kumar <54264843+rajat844@users.noreply.github.com>
2022-04-03 01:45:20 +0530

    Time: 48 ms (48.72%), Space: 14.1 MB (60.87%) - LeetHub

--- 103-binary-tree-zigzag-level-order-traversal/103-binary-tree-zigzag-level-order-traversal.py
+++ 103-binary-tree-zigzag-level-order-traversal/103-binary-tree-zigzag-level-order-traversal.py
@@ -5,43 +5,39 @@
 #         self.left = left
 #         self.right = right
 from collections import deque
-
 class Solution:
     def zigzagLevelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if root is None:
+            return 
+        
         st = deque()
-        ans = []
-        ltor = True
-        
         st.append(root)
-        
-        while len(st) > 0 :
-            smallans = []
+        ltor = True
+        ans = []
+        while len(st) > 0:
             temp = deque()
-            
             while len(st) > 0:
                 temp.append(st.popleft())
-                
+            minans = []
             if ltor == True:
                 while len(temp) > 0:
                     node = temp.popleft()
-                    if node:
-                        smallans.append(node.val)
+                    if node is not None:
+                        minans.append(node.val)
                         st.append(node.left)
                         st.append(node.right)
-            
             else:
                 while len(temp) > 0:
                     node = temp.popleft()
-                    if node:
-                        smallans.append(node.val)
+                    if node is not None:
+                        minans.append(node.val)
                         st.append(node.right)
                         st.append(node.left)
             
-            ltor = not ltor
+            if len(minans) > 0:
+                ans.append(minans)
             st.reverse()
-            if len(smallans) > 0:
-                ans.append(smallans)
+            ltor = not ltor
         
         return ans
-                    
-        
+                

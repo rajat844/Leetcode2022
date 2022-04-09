6392ed7e768bd2199fa02cbe082cb088b85f82f7
Rajat Kumar <54264843+rajat844@users.noreply.github.com>
2022-04-09 23:39:34 +0530

    Added solution - LeetHub

---
+++ Convert array into Zig-Zag fashion - GFG/convert-array-into-zig-zag-fashion.py
@@ -0,0 +1,36 @@
+#User function Template for python3
+class Solution:
+
+	def zigZag(self,arr, n):
+		# code here
+		left = True
+		
+		for i in range(1,n):
+		    if left == True and arr[i] < arr[i-1]:
+		        arr[i],arr[i-1] = arr[i-1],arr[i]
+		    if left == False and arr[i] > arr[i-1]:
+		        arr[i],arr[i-1] = arr[i-1],arr[i]
+		    
+		    left = not left
+		
+		return arr
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+
+
+
+if __name__ == '__main__':
+    tc = int(input())
+    while tc > 0:
+        n = int(input())
+        arr = list(map(int, input().strip().split()))
+        ob = Solution()
+        ob.zigZag(arr, n)
+        for x in arr:
+            print(x, end=" ")
+        print()
+        tc -= 1
+
+# } Driver Code Ends

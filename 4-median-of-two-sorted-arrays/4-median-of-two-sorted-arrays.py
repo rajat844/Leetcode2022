bd629776e55b38872175ff234f709b483f3a623c
Rajat Kumar <54264843+rajat844@users.noreply.github.com>
2022-04-14 14:13:44 +0530

    Time: 98 ms (85.80%), Space: 14.3 MB (28.60%) - LeetHub

--- 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.py
+++ 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.py
@@ -1,35 +1,40 @@
 import math
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        if len(nums1) > len(nums2) :
+        if len(nums1) > len(nums2):
             return self.findMedianSortedArrays(nums2,nums1)
-        
-        n = len(nums1)
-        m = len(nums2)
-        md = (n+m+1)>>1
+        n1 = len(nums1)
+        n2 = len(nums2)
         l = 0
-        h = n
+        h = n1
+        md = (n1+n2+1)//2
+        
         
         while l <= h:
-            cut1 = (h+l)>>1
-            cut2 = md - cut1
+            mid1 = (l+h)//2
+            mid2 = md - mid1
             
-            l1 = -math.inf if cut1 == 0 else nums1[cut1-1]
-            l2 = -math.inf if cut2 == 0 else nums2[cut2-1]
-            r1 = math.inf if cut1 == n else nums1[cut1]
-            r2 = math.inf if cut2 == m else nums2[cut2]
+            l1 = -math.inf if mid1 <= 0 else nums1[mid1 - 1]
+            l2 = -math.inf if mid2 <= 0 else nums2[mid2 - 1]
+            
+            r1 = math.inf if mid1 == n1 else nums1[mid1]
+            r2 = math.inf if mid2 == n2 else nums2[mid2]
             
             if l1 <= r2 and l2 <= r1:
-                if (m+n)%2 == 0:
-                    return (max(l1,l2)+min(r1,r2))/2
-                else :
+                if (n1+n2)%2 == 0:
+                    mn = max(l1,l2)
+                    mx = min(r1,r2)
+                    return (mn + mx)/2
+                else:
                     return max(l1,l2)
-            elif l1 > r2 :
-                h = cut1-1
+            elif l1 >= r2:
+                h = mid1 -1 
             else :
-                l = cut1+1
-                
+                l = mid1 + 1
+        
         return 0.0
+            
+            
         
         
         

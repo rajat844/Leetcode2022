da11a0fed496b391fa432b2097fb10e41332c8fd
Rajat Kumar <54264843+rajat844@users.noreply.github.com>
2022-04-16 19:30:10 +0530

    Added solution - LeetHub

--- N meetings in one room - GFG/n-meetings-in-one-room.py
+++ N meetings in one room - GFG/n-meetings-in-one-room.py
@@ -7,20 +7,18 @@ class Solution:
     def maximumMeetings(self,n,start,end):
         # code here
         st = []
-        
         for i in range(n):
             st.append((start[i],end[i]))
-            
-        st = sorted(st, key = lambda x:x[1])
         
-        curr = 0
-        count = 0
+        st.sort(key = lambda x : x[1])
+        
+        e = 0
+        ans = 0
         for i in range(n):
-            if st[i][0] > curr:
-                count += 1
-                curr = st[i][1]
-                
-        return count
+            if st[i][0] > e:
+                ans += 1
+                e = st[i][1]
+        return ans
 
 #{ 
 #  Driver Code Starts

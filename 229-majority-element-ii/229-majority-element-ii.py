410a47460bfd213755b8d0e3a253dce1499866fe
Rajat Kumar <54264843+rajat844@users.noreply.github.com>
2022-04-18 20:03:50 +0530

    Time: 128 ms (77.35%), Space: 15.3 MB (26.70%) - LeetHub

--- 229-majority-element-ii/229-majority-element-ii.py
+++ 229-majority-element-ii/229-majority-element-ii.py
@@ -1,45 +1,43 @@
 import math
 class Solution:
     def majorityElement(self, nums: List[int]) -> List[int]:
-        ele1 = math.inf
-        ele2 = math.inf
+        ele1 = None
+        ele2 = None
+        
         count1 = 0
         count2 = 0
         
         for i in range(len(nums)):
-           
-            if ele1 == nums[i]:
-                count1  += 1
-            
-            elif ele2 == nums[i]:
+            if nums[i] == ele1:
+                count1 += 1
+
+            elif nums[i] == ele2:
                 count2 += 1
                 
             elif count1 == 0:
-                count1 += 1
                 ele1 = nums[i]
-            
-            elif count2 == 0:
-                count2 += 1
-                ele2 = nums[i]
+                count1 = 1
                 
+            elif count2 == 0:
+                count2 = 1
+                ele2 = nums[i]
             else:
                 count1 -= 1
                 count2 -= 1
-        
-        ans = []
         x = 0
         y = 0
         for i in range(len(nums)):
-            if ele1 == nums[i]:
-                x += 1
-            elif ele2 == nums[i]:
+            if nums[i] == ele1:
+                x+=1
+            if nums[i]== ele2:
                 y += 1
         
-        if x > len(nums)//3:
+        ans = []
+        if x > len(nums)/3:
             ans.append(ele1)
-        if y > len(nums)//3:
+        if y > len(nums)/3:
             ans.append(ele2)
             
         return ans
-            
+        
                 

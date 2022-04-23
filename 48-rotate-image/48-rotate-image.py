03db797fd7846e5444019d2ec1908332ecb015cd
Rajat Kumar <54264843+rajat844@users.noreply.github.com>
2022-04-23 21:21:40 +0530

    Time: 53 ms (40.27%), Space: 14 MB (31.74%) - LeetHub

--- 48-rotate-image/48-rotate-image.py
+++ 48-rotate-image/48-rotate-image.py
@@ -4,11 +4,10 @@ class Solution:
         Do not return anything, modify matrix in-place instead.
         """
         n = len(matrix)
-        
         for i in range(n):
             for j in range(i):
                 matrix[i][j],matrix[j][i] = matrix[j][i],matrix[i][j]
-                
+        
         for i in range(n):
             matrix[i] = matrix[i][::-1]
-                
+        

ac65047347765b8a6028c0b2cfbc4180c39145c4
Rajat Kumar <54264843+rajat844@users.noreply.github.com>
2022-04-27 20:47:51 +0530

    Time: 3350 ms (22.54%), Space: 30.2 MB (32.27%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.py
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.py
@@ -1,28 +1,27 @@
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
-        sums = sum(nums)
-        
-        if sums %2 != 0:
+        s = sum(nums)
+        s = s/2
+        if s != int(s):
             return False
         
-        sums = int(sums/2)
+        s = int(s)
         
-        n = len(nums)
+        dp = [[False for i in range(s+1)] for j in range(len(nums)+1)]
         
-        dp = [[None for j in range(sums+1)]for i in range(n+1)]
-        
-        for j in range(sums+1):
+        for j in range(s+1):
             dp[0][j] = False
         
-        for i in range(n+1):
+        for i in range(len(nums) + 1):
             dp[i][0] = True
-            
-        for i in range(1,n+1):
-            for j in range(1,sums+1):
-                
-                if j >= nums[i-1]:
-                    dp[i][j] = dp[i-1][j-nums[i-1]] or dp[i-1][j]
+
+        for i in range(1,len(nums) + 1):
+            for j in range(1,s+1):
+                if j >= nums[i - 1]:
+                    dp[i][j] = dp[i-1][j] or dp[i-1][j-nums[i-1]]
                 else :
                     dp[i][j] = dp[i-1][j]
+        
+        return dp[len(nums)][s]
                     
-        return dp[n][sums]
+        

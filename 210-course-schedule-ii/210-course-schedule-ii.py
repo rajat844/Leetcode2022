2a9e14ce114fbb1918bce8df2ba1e26971b49e5e
Rajat Kumar <54264843+rajat844@users.noreply.github.com>
2022-04-27 20:05:24 +0530

    Time: 154 ms (38.05%), Space: 15.5 MB (87.66%) - LeetHub

--- 210-course-schedule-ii/210-course-schedule-ii.py
+++ 210-course-schedule-ii/210-course-schedule-ii.py
@@ -1,33 +1,39 @@
+from collections import deque
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
-        adj = defaultdict(list)
-
+        #adjacency list
+        adj = {}
+        for i in range(numCourses):
+            adj[i] = []
+            
         for i in range(len(prerequisites)):
-            adj[prerequisites[i][0]].append(prerequisites[i][1])
-            
-        visited = [False for i in range(numCourses)]
-        dfsvisited = [False for i in range(numCourses)]
-
-        def helper(node): 
-            visited[node] = True
-            dfsvisited[node] = True
-            
-            for x in adj[node]:
-                if visited[x] == False:
-                    if helper(x) == True:
-                        return True
-                elif dfsvisited[x] == True:
-                    return True
-            
-            dfsvisited[node] = False
-            ans.append(node)
-            return False
+            adj[prerequisites[i][1]].append(prerequisites[i][0])
         
+        #count the courses need to done before any 
+        count = [0 for i in range(numCourses)]
+        
+       
+        for i in range(numCourses):
+            for x in adj[i]:
+                count[x] += 1
+                
+        st = deque()
+        
+         #pushed all courses with prerequisites is 0
+        for i in range(numCourses):
+            if count[i] == 0:
+                st.append(i)
         
         ans = []
-        for i in range(numCourses):
-            if visited[i] == False:
-                if helper(i) == True:
-                    return []
-        
-        return ans
+        while len(st) > 0:
+            node = st.popleft()
+            ans.append(node)
+            
+            for x in adj[node]:
+                count[x] -= 1
+                if count[x] == 0:
+                    st.append(x)
+        if len(ans) == numCourses:
+            return ans
+        return []
+            

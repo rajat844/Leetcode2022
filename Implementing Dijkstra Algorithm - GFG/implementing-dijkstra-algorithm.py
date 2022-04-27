f37be430cf0fef57aed6a7f615bdfcf8a887949d
Rajat Kumar <54264843+rajat844@users.noreply.github.com>
2022-04-27 20:38:33 +0530

    Added solution - LeetHub

--- Implementing Dijkstra Algorithm - GFG/implementing-dijkstra-algorithm.py
+++ Implementing Dijkstra Algorithm - GFG/implementing-dijkstra-algorithm.py
@@ -1,25 +1,30 @@
 import heapq,math
 class Solution:
+
     #Function to find the shortest distance of all the vertices
     #from the source vertex S.
     def dijkstra(self, V, adj, S):
         #code here
         ans = [math.inf for i in range(V)]
-        st = [(0,S)]
         ans[S] = 0
+        st = []
+        st.append((0,S))
         heapq.heapify(st)
         
         while len(st) > 0:
-            distance,node = heapq.heappop(st)
-            
-            if distance != math.inf:
-                for x in adj[node]:
-                    if ans[x[0]] > distance + x[1]:
-                        ans[x[0]] = distance + x[1]
-                        heapq.heappush(st,(ans[x[0]],x[0]))
-                        
+            dist,node = heapq.heappop(st)
+            for x in adj[node]:
+                currnode = x[0]
+                currdist = x[1]
+                
+                if ans[currnode] > currdist + dist:
+                    ans[currnode] = currdist + dist
+                    heapq.heappush(st,(currdist + dist,currnode))
+                    
         return ans
-            
+                
+        
+        
 #{ 
 #  Driver Code Starts
 #Initial Template for Python 3

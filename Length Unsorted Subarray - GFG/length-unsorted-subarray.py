ca81aa903040b3caccc7acca9c552f9c36e40cc6
Rajat Kumar <54264843+rajat844@users.noreply.github.com>
2022-04-28 20:15:53 +0530

    Added solution - LeetHub

--- Length Unsorted Subarray - GFG/length-unsorted-subarray.py
+++ Length Unsorted Subarray - GFG/length-unsorted-subarray.py
@@ -3,23 +3,21 @@ class Solution:
 
 	def printUnsorted(self,arr, n):
 		# code here
+		mx = arr[0]
 		end = 0
-		mn = arr[0]
-		
-		for i in range(1,n):
-		    if mn > arr[i]:
+		for i in range(n):
+		    if arr[i] < mx:
 		        end = i
 		    else :
-		        mn = arr[i]
-		
+		        mx = arr[i]
+		        
+		mn = arr[-1]
 		start = 0
-		mx = arr[n-1]
-		
-		for i in range(n-2,-1,-1):
-		    if mx < arr[i]:
+		for i in range(n-1,-1,-1):
+		    if arr[i] > mn:
 		        start = i
 		    else :
-		        mx = arr[i]
+		        mn = arr[i]
 		        
 		return [start,end]
 #{ 

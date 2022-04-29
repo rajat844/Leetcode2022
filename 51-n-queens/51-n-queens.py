81d13a3311e05f2777a39d36521cf3edd75ed0f3
Rajat Kumar <54264843+rajat844@users.noreply.github.com>
2022-04-30 01:22:22 +0530

    Time: 163 ms (22.96%), Space: 14.4 MB (49.24%) - LeetHub

--- 51-n-queens/51-n-queens.py
+++ 51-n-queens/51-n-queens.py
@@ -1,42 +1,48 @@
 class Solution:
     def solveNQueens(self, n: int) -> List[List[str]]:
-        ans = []
-        board = [["."for i in range(n)]for i in range(n)]
-        
-        def isSafe(row,col):
-            tem_row,tem_col = row,col
-            while row >= 0 and col >= 0:
-                if board[row][col] == 'Q':
+        def valid(i,j):
+            row = i
+            column = j
+            
+            while row >= 0 and column >= 0:
+                if s[row][column] == "Q":
                     return False
                 row -= 1
-                col -= 1
+                column -= 1
             
-            row,col = tem_row,tem_col
-            while col >= 0:
-                if board[row][col] == 'Q':
-                    return False
-                col -= 1
+            row = i
+            column = j
             
-            row,col = tem_row,tem_col
-            while row<n and col >= 0:
-                if board[row][col] == 'Q':
+            while column >= 0:
+                if s[row][column] == "Q":
                     return False
-                col -= 1
+                column -= 1
+            
+            column = j
+            
+            while column >= 0 and row < n:
+                if s[row][column] == "Q":
+                    return False
+                column -= 1
                 row += 1
             
             return True
             
-        def helper(col):
-            if n == col:
-                t = [''.join(row) for row in board]
-                ans.append(t)
+        def helper(j):
+            if j == n:
+                tempans = [''.join(x) for x in s] 
+                ans.append(tempans)
                 return 
-            for row in range(n):
-                if isSafe(row,col):
-                    board[row][col] = 'Q'
-                    helper(col+1)
-                    board[row][col] = '.'
+            
+            for i in range(n):
+                if valid(i,j):
+                    s[i][j] = "Q"
+                    helper(j+1)
+                    s[i][j] = "."
+                    
+        ans = []
+        s = [["." for i in range(n)]for j in range(n)]
         helper(0)
         return ans
-            
+                    
         

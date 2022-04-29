5dc7a3003541f5a441f04147b685eee7cd965151
Rajat Kumar <54264843+rajat844@users.noreply.github.com>
2022-04-30 01:31:47 +0530

    Time: 358 ms (68.08%), Space: 16.4 MB (80.42%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.py
+++ 200-number-of-islands/200-number-of-islands.py
@@ -1,21 +1,23 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        n = len(grid)
-        m = len(grid[0])
-        
         def helper(i,j):
-            if i >= 0 and j >= 0 and i < n and j < m and grid[i][j] == "1":
+            if 0 <= i < n and 0 <= j < m and grid[i][j] == "1":
                 grid[i][j] = "0"
                 
-                helper(i-1,j)
-                helper(i,j-1)
                 helper(i+1,j)
+                helper(i-1,j)
                 helper(i,j+1)
+                helper(i,j-1)
+            
+            
+        n = len(grid)
+        m = len(grid[0])
         
         ans = 0
         for i in range(n):
             for j in range(m):
                 if grid[i][j] == "1":
-                    helper(i,j)
                     ans += 1
+                    helper(i,j)
         return ans
+                    

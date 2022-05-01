b157e5b6ff8a0e071b6153f481256f824d35f6f0
Rajat Kumar <54264843+rajat844@users.noreply.github.com>
2022-05-01 23:49:20 +0530

    Added solution - LeetHub

--- Palindromic patitioning - GFG/palindromic-patitioning.py
+++ Palindromic patitioning - GFG/palindromic-patitioning.py
@@ -1,59 +1,42 @@
 #User function Template for python3
-import sys
+import math
 class Solution:
     def palindromicPartition(self, string):
-        # code here
-        n = len(string)
-        dp = [[-1 for j in range(n+1)]for i in range(n+1)]
-        
-        def isPalidrome(start,end):
+        def isPalindrome(start,end):
             while start < end:
                 if string[start] != string[end]:
                     return False
                 start += 1
                 end -= 1
             return True
-        
-        def solve(i,j):
+        # code here
+        def helper(i,j):
             if dp[i][j] != -1:
                 return dp[i][j]
-                
-            if i > j :
-                dp[i][j] = 0
-                return dp[i][j]
-             
-            if isPalidrome(i,j):
+            if i > j:
                 dp[i][j] = 0
                 return dp[i][j]
             
-           
-            ans = sys.maxsize
-            
-            for k in range(i,j):
+            elif isPalindrome(i,j):
+                dp[i][j] = 0
+                return dp[i][j]
                 
-                if isPalidrome(i,k) == False:
-                    continue
-                
-                if dp[i][k] != -1:
-                    temp1 = dp[i][k]
-                else :
-                    temp1 = solve(i,k)
-                
-                if dp[k+1][j] != -1:
-                    temp2 = dp[k+1][j]
-                else:
-                    temp2 = solve(k+1,j)
+            else :
+                dp[i][j] = math.inf
+                for k in range(i,j):
+                    if isPalindrome(i,k) == False:
+                        continue
+                    ans1 = dp[i][k] if dp[i][k] != -1 else helper(i,k)
+                    ans2 = dp[k+1][j] if dp[k+1][j] != -1 else helper(k+1,j)
                     
-                ans = min(ans,temp1 + temp2 + 1)
+                    dp[i][j] = min(ans1+ans2+1,dp[i][j])
                 
-            dp[i][j] = ans
-            return dp[i][j]
-                
-                
-                
-            
+                return dp[i][j]
         
-        return solve(0,n-1)
+        n = len(string)
+        dp = [[-1 for i in range(n+1)]for i in range(n+1)]
+        return helper(0,len(string) - 1)
+        
 
 #{ 
 #  Driver Code Starts

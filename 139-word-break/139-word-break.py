b4958713f26adf8b52f00573b8bf3be0cacb185d
Rajat Kumar <54264843+rajat844@users.noreply.github.com>
2022-05-03 10:41:06 +0530

    Time: 465 ms (5.14%), Space: 15 MB (5.34%) - LeetHub

--- 139-word-break/139-word-break.py
+++ 139-word-break/139-word-break.py
@@ -2,42 +2,27 @@ class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
         n = len(s)
         dp = [[None for i in range(n)]for i in range(n)]
-        
-        for i in range(n-1,-1,-1):
-            for j in range(n):
-                if i > j:
-                    dp[i][j] = False
-                elif s[i:j+1] in wordDict:
-                    dp[i][j] = True
-                else :
-                    if dp[i][j] != None:
-                        dp[i][j] = True
-                    else:
-                        for k in range(i,j):
-                            x = dp[i][k]
-                            y = dp[k+1][j]
-                            
-                            if x and y :
-                                dp[i][j] = True
-        return dp[0][n-1]
                         
-#         def helper(i,j):
-#             if i > j :
-#                 return True
+        def helper(i,j):
+            if i > j :
+                dp[i][j] = False
+                return dp[i][j]
+            if dp[i][j] != None:
+                return dp[i][j]
             
-#             if s[i:j+1] in wordDict:
-#                 return True
-            
-#             ans = False
-            
-#             for k in range(i,j):
-#                 x = helper(i,k)
-#                 y = helper(k+1,j)
+            if s[i:j+1] in wordDict:
+                dp[i][j] = True
+                return dp[i][j]
+
+            for k in range(i,j):
+                x = helper(i,k)
+                y = helper(k+1,j)
                 
-#                 if x and y:
-#                     return True
-        
-#         return helper(0,len(s) - 1)
+                if x and y:
+                    dp[i][j] = True
+                    return dp[i][j]
+            dp[i][j] = False
+        return helper(0,len(s) - 1)
         
                 
         

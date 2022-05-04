4213fbd0f8f99ebb25080cff42ec8f4e1ba8afd6
Rajat Kumar <54264843+rajat844@users.noreply.github.com>
2022-05-04 12:26:56 +0530

    Time: 41 ms (94.36%), Space: 14 MB (61.85%) - LeetHub

---
+++ 10-regular-expression-matching/10-regular-expression-matching.py
@@ -0,0 +1,29 @@
+class Solution:
+    def isMatch(self, s: str, p: str) -> bool:
+        dp = [[None for i in range(len(p))] for j in range(len(s)+1)]
+        
+        
+        def helper(i,j):
+            if j== len(p):
+                return i == len(s)
+            
+            
+            if dp[i][j] != None:
+                return dp[i][j]
+            
+            match = i < len(s) and (s[i] == p[j] or p[j] == ".")
+            
+            if j + 1 < len(p) and p[j+1] == '*':
+                dp[i][j] = helper(i,j+2) or (match and helper(i+1,j))
+            
+            elif match == True:
+                dp[i][j] =  helper(i+1,j+1)
+            
+            else :
+                dp[i][j] == False
+            
+            return dp[i][j]
+        
+        return helper(0,0)
+          
+                

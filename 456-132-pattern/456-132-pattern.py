90cd8703e9d9dceb0508867a8c01f8391f76791f
Rajat Kumar <54264843+rajat844@users.noreply.github.com>
2022-05-07 23:41:01 +0530

    Time: 362 ms (89.93%), Space: 32.1 MB (33.50%) - LeetHub

--- 456-132-pattern/456-132-pattern.py
+++ 456-132-pattern/456-132-pattern.py
@@ -7,10 +7,9 @@ class Solution:
         for i in range(len(nums) - 1,-1,-1):
             if nums[i] < s2:
                 return True
-            else:
-                while len(st) > 0 and nums[i] > st[-1]:
-                    s2 = st[-1]
-                    st.pop()
+            while len(st) > 0 and nums[i] > st[-1]:
+                s2 = st[-1]
+                st.pop()
             
             st.append(nums[i])
         

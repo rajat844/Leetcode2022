6e7536aa9d17a0cee53fdd44bd40ac9f6244054b
Rajat Kumar <54264843+rajat844@users.noreply.github.com>
2022-05-07 11:19:43 +0530

    Time: 997 ms (23.26%), Space: 22.6 MB (71.90%) - LeetHub

--- 1092-shortest-common-supersequence/1092-shortest-common-supersequence.py
+++ 1092-shortest-common-supersequence/1092-shortest-common-supersequence.py
@@ -7,39 +7,37 @@ class Solution:
         
         for i in range(n+1):
             for j in range(m+1):
-                
                 if i == 0 or j == 0:
                     dp[i][j] = 0
-                    
                 elif str1[i-1] == str2[j-1]:
                     dp[i][j] = dp[i-1][j-1] + 1
-                else:
-                    dp[i][j] = max(dp[i-1][j],dp[i][j-1])
                     
-        ans = ""
+                else :
+                    dp[i][j] = max(dp[i][j-1],dp[i-1][j])
+        
         i = n
         j = m
         
-        while i>0 and j>0:
-            if str1[i-1] == str2[j-1]:
+        ans = ''
+        
+        while i > 0 and j > 0:
+            if str1[i-1] == str2[j-1] :
                 ans += str1[i-1]
                 i -= 1
                 j -= 1
+            elif dp[i-1][j] < dp[i][j-1]:
+                ans += str2[j-1]
+                j -= 1
             else:
-                if dp[i-1][j] > dp[i][j-1]:
-                    ans += str1[i-1]
-                    i -= 1
-                else :
-                    ans += str2[j-1]
-                    j -= 1
-                    
+                ans += str1[i-1]
+                i -= 1
+        
         while i > 0:
             ans += str1[i-1]
             i -= 1
+        
         while j > 0:
             ans += str2[j-1]
             j -= 1
-            
+        
         return ans[::-1]
-        
-        

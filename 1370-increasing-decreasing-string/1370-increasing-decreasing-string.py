81de300d2b6463948d92614aa606b078aeeeb191
Rajat Kumar <54264843+rajat844@users.noreply.github.com>
2022-05-11 00:26:22 +0530

    Time: 66 ms (81.88%), Space: 13.9 MB (45.85%) - LeetHub

--- 1370-increasing-decreasing-string/1370-increasing-decreasing-string.py
+++ 1370-increasing-decreasing-string/1370-increasing-decreasing-string.py
@@ -1,23 +1,20 @@
 class Solution:
     def sortString(self, s: str) -> str:
-        arr = [0] * 26
+        arr = [0 for i in range(26)]
         
         for i in range(len(s)):
-            arr[ord(s[i]) - ord('a')] += 1
+            arr[ ord(s[i]) - ord('a')] += 1
             
         ans = ''
-        while max(arr) > 0 :
-            for i in range(len(arr)):
+        while max(arr) > 0:
+            for i in range(26):
                 if arr[i] > 0:
                     arr[i] -= 1
-                    ans += chr(i + 97)
+                    ans += chr(97+i)
             
-            for i in range(len(arr) - 1 , -1,-1):
+            for i in range(25,-1,-1):
                 if arr[i] > 0:
                     arr[i] -= 1
-                    ans += chr( 97 + i)
-        
+                    ans += chr(97+i)
+                    
         return ans
-            
-        
-        

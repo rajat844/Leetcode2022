41ef27a77e35d9401ab4e9e28f6ae9c89d9c4ffe
Rajat Kumar <54264843+rajat844@users.noreply.github.com>
2022-05-16 20:16:20 +0530

    Time: 108 ms (23.18%), Space: 17.5 MB (5.15%) - LeetHub

--- 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.py
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.py
@@ -1,18 +1,30 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         n = len(prices)
+        dp = [[-1 for j in range(2)] for i in range(n+1)]
         
-        dp = [[-1 for i in range(2)] for j in range(n+1)]
         dp[n][0] = dp[n][1] = 0
-        
-        
+    
         for i in range(n-1,-1,-1):
-            for buy in range(0,2,1):
-                if buy == 1:
-                    dp[i][buy] = max(-prices[i] + dp[i+1][0],dp[i+1][1])
-                else :
-                    dp[i][buy] = max(prices[i] + dp[i+1][1], dp[i+1][0])
+            dp[i][1] = max(-1*prices[i]+dp[i+1][0],dp[i+1][1])
+            dp[i][0] = max(prices[i] + dp[i+1][1],dp[i+1][0])
         
         return dp[0][1]
-
+            
+#         def helper(i,b):
+#             if i == len(prices):
+#                 return 0
+#             case1 = 0
+#             case2 = 0
+#             if b == True:
+#                 case1 = -1*prices[i] + helper(i+1,False)
+#                 case2 = helper(i+1,True)
+#             else:
+#                 case1 = prices[i] + helper(i+1,True)
+#                 case2 = helper(i+1,False)
+            
+#             return max(case1,case2)
+        
+#         return helper(0,True)
+                
         

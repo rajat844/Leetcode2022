3a4a707c7e6cefdc337fb605412382d994408953
Rajat Kumar <54264843+rajat844@users.noreply.github.com>
2022-05-19 04:40:39 +0530

    Added solution - LeetHub

--- Get Minimum Squares - GFG/get-minimum-squares.py
+++ Get Minimum Squares - GFG/get-minimum-squares.py
@@ -1,25 +1,21 @@
-#User function Template for python
-from math import sqrt,ceil
-import sys
+#User function Template for python3
+import math 
+
 class Solution:
 	def MinSquares(self, n):
 		# Code here
-		dp = [0 for i in range(n+1)]
+		dp = [math.inf for i in range(n+1)]
 		
 		dp[0] = 0
 		dp[1] = 1
 		
 		for i in range(2,n+1):
-		    dp[i] = sys.maxsize
-		    for j in range(1,ceil(sqrt(i)) + 1):
-		        if j * j <= i:
-		            dp[i] = min(dp[i],dp[i-j*j]+1)
-		        
-		return dp[n]
-		    
-		    
-		    
+		    for j in range(1,int(math.sqrt(i)) + 1):
+		        if j * j <= i and dp[i] > dp[i-j*j] + 1:
+		            dp[i] = dp[i - j*j] + 1
 		
+		return dp[n]
+
 #{ 
 #  Driver Code Starts
 #Initial Template for Python 3

2cd9fe15dec9b2ac7fa81c2eab8e04e40a4179ef
Rajat Kumar <54264843+rajat844@users.noreply.github.com>
2022-05-31 23:41:19 +0530

    Time: 233 ms (20.98%), Space: 18 MB (46.65%) - LeetHub

--- 23-merge-k-sorted-lists/23-merge-k-sorted-lists.py
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.py
@@ -4,34 +4,42 @@
 #         self.val = val
 #         self.next = next
 class Solution:
+    def merge(self,l1,l2):
+        head = temp = ListNode(0)
+            
+        while l1 and l2 :
+            if l1.val > l2.val:
+                temp.next = l2
+                l2 = l2.next
+                temp = temp.next
+            else:
+                temp.next = l1
+                l1 = l1.next
+                temp = temp.next
+        if l1 or l2:
+            temp.next = l1 or l2
+        
+        return head.next
+            
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
         if len(lists) == 0:
             return None
-        n  = len(lists)
-        i = n-1
         
-        while i > 0:
-            l1 = lists[i]
-            l2 = lists[i-1]
-            
-            head = temp = ListNode(0)
-            
-            while l1 and l2 :
-                if l1.val > l2.val:
-                    temp.next = l2
-                    l2 = l2.next
-                    temp = temp.next
-                else:
-                    temp.next = l1
-                    l1 = l1.next
-                    temp = temp.next
-            if l1 or l2:
-                temp.next = l1 or l2
+        while len(lists) > 1:
+            newLists = []
+            for i in range(0,len(lists),2):
+                l1 = lists[i]
+                l2 = lists[i+1] if i+1 < len(lists) else None
                 
-            lists[i-1] = head.next
-            i -= 1
+                merged = self.merge(l1,l2)
+                newLists.append(merged)
+                
+            lists = newLists
         
         return lists[0]
-            
+                
+                
+        
+ 
             
         

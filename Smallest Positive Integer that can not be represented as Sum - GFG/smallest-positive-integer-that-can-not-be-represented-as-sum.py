37c7e0cbddd257e6dc7bbbd1e049d57c6f21b5fd
Rajat Kumar <54264843+rajat844@users.noreply.github.com>
2022-06-02 10:51:09 +0530

    Added solution - LeetHub

--- Smallest Positive Integer that can not be represented as Sum - GFG/smallest-positive-integer-that-can-not-be-represented-as-sum.py
+++ Smallest Positive Integer that can not be represented as Sum - GFG/smallest-positive-integer-that-can-not-be-represented-as-sum.py
@@ -1,21 +1,20 @@
 #User function Template for python3
 
 class Solution:
-    def smallestpositive(self, array, n): 
+    def smallestpositive(self, arr, n): 
         # Your code goes here  
-        array.sort()
-        target = 1
+        arr.sort()
         s = 0
+        target = 1
+        
         for i in range(n):
-            if array[i] <= target:
-                s += array[i]
-                target = s + 1
+            if arr[i] <= target:
+                s += arr[i]
+                target = s+1
             else:
                 break
         
         return target
-            
-        
 
 #{ 
 #  Driver Code Starts

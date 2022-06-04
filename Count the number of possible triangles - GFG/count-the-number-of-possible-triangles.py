f4d6cec739b18916dc26e4ed29e705b1ee881b9b
Rajat Kumar <54264843+rajat844@users.noreply.github.com>
2022-06-04 20:16:22 +0530

    Added solution - LeetHub

---
+++ Count the number of possible triangles - GFG/count-the-number-of-possible-triangles.py
@@ -0,0 +1,36 @@
+#User function Template for python3
+
+class Solution:
+    #Function to count the number of possible triangles.
+    def findNumberOfTriangles(self, arr, n):
+        #code here
+        arr.sort()
+        ans = 0
+        
+        for c in range(n-1,1,-1):
+            a = 0
+            b = c - 1
+            
+            while a < b:
+                if arr[a] + arr[b] > arr[c]:
+                    ans += b-a
+                    b -= 1
+                else:
+                    a += 1
+        
+        return ans
+                
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__': 
+    t = int (input ())
+    for _ in range (t):
+        n = int(input())
+        arr = list(map(int, input().strip().split()))
+        ob = Solution()
+        print(ob.findNumberOfTriangles(arr,n))
+
+# } Driver Code Ends

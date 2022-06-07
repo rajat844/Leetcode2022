f3f3a78863717533abfbaa378c3366e6662b0d1c
Rajat Kumar <54264843+rajat844@users.noreply.github.com>
2022-06-07 12:34:41 +0530

    Time: 1190 ms (29.11%), Space: 23 MB (43.13%) - LeetHub

--- 44-wildcard-matching/44-wildcard-matching.py
+++ 44-wildcard-matching/44-wildcard-matching.py
@@ -2,7 +2,7 @@ class Solution:
     def isMatch(self, s: str, p: str) -> bool:
         n = len(s)
         m = len(p)
-        dp = [[None for j in range(m+1)]for i in range(n+1)]
+        dp = [[None for i in range(m+1)]for j in range(n+1)]
         
         dp[0][0] = True
         
@@ -18,42 +18,13 @@ class Solution:
         
         for i in range(1,n+1):
             for j in range(1,m+1):
-                
                 if p[j-1] == "*":
                     dp[i][j] = dp[i-1][j] or dp[i][j-1]
-                
-                elif s[i-1] == p[j-1] or  p[j-1] == "?":
+                                                   
+                elif s[i-1] == p[j-1] or p[j-1] == "?":
                     dp[i][j] = dp[i-1][j-1]
                     
-                else:
+                else :
                     dp[i][j] = False
         
         return dp[n][m]
-        
-        
-        
-        
-        
-        
-        
-#         def matching(s_idx,p_idx):
-#             if p_idx < 0 and s_idx < 0:
-#                 return True
-#             if p_idx < 0 and s_idx >= 0:
-#                 return False
-            
-#             if s_idx < 0 and p_idx >= 0 :
-#                 for i in range(p_idx+1):
-#                     if p[i] != "*":
-#                         return False
-#                 return True
-            
-#             if s[s_idx] == p[p_idx] or p[p_idx] == "?":
-#                 return matching(s_idx-1,p_idx-1)
-            
-#             if p[p_idx] == "*":
-#                 return matching(s_idx-1,p_idx) or matching(s_idx,p_idx-1)
-            
-#             return False
-        
-#         return matching(len(s)-1,len(p)-1)

c71cf0d6d11e486b0b233e98f2cf949e5148307c
Rajat Kumar <54264843+rajat844@users.noreply.github.com>
2022-06-13 20:57:18 +0530

    Time: 143 ms (8.75%), Space: 15.3 MB (20.24%) - LeetHub

--- 120-triangle/120-triangle.py
+++ 120-triangle/120-triangle.py
@@ -4,18 +4,27 @@ class Solution:
         n = len(triangle)
         dp = [[-1 for i in range(m)]for j in range(n)]
         
-        def recursion(i,j):
-            if dp[i][j] != -1:
-                return dp[i][j]
-            
-            if i == len(triangle) - 1:
-                dp[i][j] =  triangle[i][j]
-                return dp[i][j]
-            
-            dp[i][j] = triangle[i][j] + min(recursion(i+1,j),recursion(i+1,j+1))
-            return dp[i][j]
+        for i in range(m):
+            dp[n-1][i] = triangle[n-1][i]
         
-        return recursion(0,0)
+        for i in range(n-2,-1,-1):
+            for j in range(len(triangle[i])-1,-1,-1):
+                dp[i][j] = triangle[i][j] + min(dp[i+1][j],dp[i+1][j+1])
+        
+        return dp[0][0]
+        
+#         def recursion(i,j):
+#             if dp[i][j] != -1:
+#                 return dp[i][j]
+            
+#             if i == len(triangle) - 1:
+#                 dp[i][j] =  triangle[i][j]
+#                 return dp[i][j]
+            
+#             dp[i][j] = triangle[i][j] + min(recursion(i+1,j),recursion(i+1,j+1))
+#             return dp[i][j]
+        
+#         return recursion(0,0)
                 
             
             

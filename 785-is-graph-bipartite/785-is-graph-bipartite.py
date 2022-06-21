4c558dc29fdb46f270b2fe4a58aeafe0f9678a44
Rajat Kumar <54264843+rajat844@users.noreply.github.com>
2022-06-21 23:15:11 +0530

    Time: 355 ms (12.16%), Space: 14.6 MB (35.27%) - LeetHub

--- 785-is-graph-bipartite/785-is-graph-bipartite.py
+++ 785-is-graph-bipartite/785-is-graph-bipartite.py
@@ -1,26 +1,30 @@
+from collections import deque
 class Solution:
     def isBipartite(self, graph: List[List[int]]) -> bool:
-        def dfs(node,color):
-            visited[node] = True
-            colors[node] = color
-            
-            for x in graph[node]:
-                if visited[x] == False:
-                    if dfs(x,1-color) == False:
-                        return False
-                elif colors[x] == color:
-                    return False
-            return True
-        
         n = len(graph)
-        colors = [-1 for i in range(n)]
         visited = [False for i in range(n)]
+        colors = [-1 for i in range(n)]
         
         for i in range(n):
             if visited[i] == False:
-                if dfs(i,0) == False:
-                    return False
+                visited[i] = True
+                st = deque()
+                st.append((i,0))
+                
+                while len(st) > 0:
+                    node,color = st.popleft()
+                    colors[node] = color
+                    
+                    for x in graph[node]:
+                        if visited[x] == False:
+                            visited[x] = True
+                            st.append((x,1-color))
+                        elif colors[x] == color:
+                            return False
         
         return True
-            
+                    
+                    
+                
+        
         

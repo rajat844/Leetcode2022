49f780a0a1a904cf5873a046e8fc3005747dac13
Rajat Kumar <54264843+rajat844@users.noreply.github.com>
2022-06-30 01:29:22 +0530

    Time: 2701 ms (7.06%), Space: 60.7 MB (6.01%) - LeetHub

--- 1499-max-value-of-equation/1499-max-value-of-equation.py
+++ 1499-max-value-of-equation/1499-max-value-of-equation.py
@@ -1,25 +1,21 @@
-import math
 from collections import deque
+import math
 class Solution:
-    def findMaxValueOfEquation(self, points: List[List[int]], k: int) -> int:
-        n = len(points)
-        ans = -math.inf
-        
+    def findMaxValueOfEquation(self, p: List[List[int]], k: int) -> int:
         st = deque()
-        
+        ans = -math.inf
+        n = len(p)
         for i in range(n):
-            while len(st) > 0 and points[i][0] - points[st[0]][0] > k:
+            while len(st) > 0 and  p[i][0] - p[st[0]][0] > k:
                 st.popleft()
             
-            if len(st) > 0:
-                ans = max(ans,points[st[0]][1] - points[st[0]][0] + points[i][1] + points[i][0])
-                
-            while len(st) > 0 and points[st[-1]][1] - points[st[-1]][0] < points[i][1] - points[i][0]:
+            if len(st) > 0 :
+                ans = max(ans,p[st[0]][1] - p[st[0]][0] + p[i][1] + p[i][0])
+            
+            while len(st) > 0 and p[st[-1]][1] - p[st[-1]][0] < p[i][1] - p[i][0]:
                 st.pop()
             
             st.append(i)
         
         return ans
-                                         
-                                         
-                
+        

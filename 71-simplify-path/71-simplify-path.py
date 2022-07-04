ef064381699d641bfe47c2943ac0addf6c1a4551
Rajat Kumar <54264843+rajat844@users.noreply.github.com>
2022-07-04 20:44:13 +0530

    Time: 69 ms (13.22%), Space: 13.8 MB (80.13%) - LeetHub

--- 71-simplify-path/71-simplify-path.py
+++ 71-simplify-path/71-simplify-path.py
@@ -2,15 +2,17 @@ class Solution:
     def simplifyPath(self, path: str) -> str:
         st = []
         curr = ""
-        for ch in path + "/":
-            if ch == "/":
+        
+        path += "/"
+        for i in range(len(path)):
+            if path[i] == "/":
                 if curr == "..":
-                    if len(st) > 0 :
+                    if len(st) > 0:
                         st.pop()
                 elif curr != "" and curr != ".":
                     st.append(curr)
                 curr = ""
             else:
-                curr += ch
+                curr += path[i]
         
-        return "/" + "/".join(st)
+        return "/"+"/".join(st)

5bd1c675b16b029b7bb2cd46ec4bed168eea5104
Rajat Kumar <54264843+rajat844@users.noreply.github.com>
2022-07-04 08:41:30 +0530

    Time: 1406 ms (80.55%), Space: 60.4 MB (77.13%) - LeetHub

--- 1499-max-value-of-equation/1499-max-value-of-equation.py
+++ 1499-max-value-of-equation/1499-max-value-of-equation.py
@@ -2,17 +2,17 @@ from collections import deque
 import math
 class Solution:
     def findMaxValueOfEquation(self, p: List[List[int]], k: int) -> int:
-        st = deque()
         ans = -math.inf
-        n = len(p)
-        for i in range(n):
-            while len(st) > 0 and  p[i][0] - p[st[0]][0] > k:
+        st = deque()
+        for i in range(len(p)):
+            while len(st) > 0 and p[i][0] - p[st[0]][0] > k:
                 st.popleft()
             
-            if len(st) > 0 :
-                ans = max(ans,p[st[0]][1] - p[st[0]][0] + p[i][1] + p[i][0])
-            
-            while len(st) > 0 and p[st[-1]][1] - p[st[-1]][0] < p[i][1] - p[i][0]:
+            if len(st) > 0:
+                x = p[i][0] + p[i][1] - p[st[0]][0] + p[st[0]][1]
+                ans = max(ans,x)
+                
+            while len(st) > 0 and p[st[-1]][0]-p[st[-1]][1] > p[i][0] - p[i][1]:
                 st.pop()
             
             st.append(i)

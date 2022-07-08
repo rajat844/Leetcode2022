07e42ab8776b41991c8be5c52444a1dcccfd86a2
Rajat Kumar <54264843+rajat844@users.noreply.github.com>
2022-07-09 00:25:47 +0530

    Time: 62 ms (82.49%), Space: 14.1 MB (29.28%) - LeetHub

--- 2327-number-of-people-aware-of-a-secret/2327-number-of-people-aware-of-a-secret.py
+++ 2327-number-of-people-aware-of-a-secret/2327-number-of-people-aware-of-a-secret.py
@@ -1,21 +1,18 @@
-from collections import deque
 class Solution:
     def peopleAwareOfSecret(self, n: int, delay: int, forget: int) -> int:
-        dp = [None for i in range(n)]
+        dp = [0 for i in range(n)]
         dp[0] = 1
         prev = 0
         
         for i in range(1,n):
-            x = dp[i-forget] if i - forget >= 0 else 0
-            y = dp[i-delay] if i -delay >= 0 else 0
-            x = max(x,0)
-            y = max(y,0)
+            fr_ppl = 0 if (i - forget < 0 or dp[i-forget] < 0) else dp[i-forget]
+            dl_ppl = 0 if (i - delay < 0 or dp[i-delay] < 0) else dp[i-delay]
             
-            dp[i] = prev + y - x
+            dp[i] = prev + dl_ppl - fr_ppl
             prev = dp[i]
         
-        s = 0
+        ans = 0
         for i in range(forget):
-            s += dp[n-i-1]
+            ans += dp[n-i-1]
         
-        return s % (10**9+7)
+        return ans % (10**9 + 7)

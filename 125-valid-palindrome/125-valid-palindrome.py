099a1e35b06edb24851d8e96db074d6ec2915951
Rajat Kumar <54264843+rajat844@users.noreply.github.com>
2022-07-15 09:55:01 +0530

    Time: 173 ms (5.03%), Space: 14.7 MB (36.51%) - LeetHub

---
+++ 125-valid-palindrome/125-valid-palindrome.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        t = ""
+        for i in range(len(s)):
+            if ord("0") <= ord(s[i]) <= ord("9"):
+                t += s[i]
+            elif ord("A") <= ord(s[i]) <= ord("Z"):
+                t += s[i].lower()
+            elif ord("a") <= ord(s[i]) <= ord("z"):
+                t += s[i]
+        print(t)
+        if t == t[::-1]:
+            return True
+        return False

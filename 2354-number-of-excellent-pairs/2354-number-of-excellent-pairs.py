fd2b0b53129790e496274de26924c85fc8d3bef3
Rajat Kumar <54264843+rajat844@users.noreply.github.com>
2022-07-26 01:01:04 +0530

    Time: 792 ms (100.00%), Space: 32 MB (75.00%) - LeetHub

--- 2354-number-of-excellent-pairs/2354-number-of-excellent-pairs.py
+++ 2354-number-of-excellent-pairs/2354-number-of-excellent-pairs.py
@@ -2,16 +2,16 @@ from collections import defaultdict
 class Solution:
     def countExcellentPairs(self, nums: List[int], k: int) -> int:
         st = defaultdict(int)
-        ans = 0
         s = set(nums)
         
         for x in s:
-            c = bin(x).count("1")
-            st[c] += 1
-        
+            cnt = bin(x).count("1")
+            st[cnt] += 1
+            
+        ans = 0
         for i in range(1,30):
             for j in range(1,30):
                 if i + j >= k:
                     ans += st.get(i,0) * st.get(j,0)
-        return ans
         
+        return ans

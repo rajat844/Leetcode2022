cc1ca793a7f4a07a62b3cfaa6d54981084910a03
Rajat Kumar <54264843+rajat844@users.noreply.github.com>
2022-07-26 18:24:30 +0530

    Time: 75 ms (96.06%), Space: 13.9 MB (77.92%) - LeetHub

--- 698-partition-to-k-equal-sum-subsets/698-partition-to-k-equal-sum-subsets.py
+++ 698-partition-to-k-equal-sum-subsets/698-partition-to-k-equal-sum-subsets.py
@@ -1,33 +1,35 @@
 class Solution:
     def canPartitionKSubsets(self, nums: List[int], k: int) -> bool:
-        def dfs(made,curr,start,limit):
-            if made == k:
+        def dfs(idx,curr,cnt,limit):
+            if cnt == k:
                 return True
-            if curr == limit:
-                return dfs(made+1,0,0,limit)
-            i = start
+            if curr ==limit:
+                return dfs(0,0,cnt+1,limit)
+            
+            i = idx
             while i < len(nums):
                 if visited[i] or nums[i]+curr > limit:
                     i += 1
                     continue
                 visited[i] = True
-                if dfs(made,curr+nums[i],i+1,limit):
+                if dfs(i+1,curr+nums[i],cnt,limit):
                     return True
                 visited[i] = False
                 
-                while i+1 < len(nums) and nums[i+1] == nums[i]:
+                while i+1 < len(nums) and nums[i] == nums[i+1]:
                     i += 1
-
+                if curr == 0 or curr + nums[i] == limit:
+                    return False
                 i += 1
-                
             return False
         
         if len(nums) < k or sum(nums) % k:
             return False
         numSum = sum(nums)
-        nums.sort(reverse = True)
-        for x in nums:
-            if x > numSum // k:
+        
+        for  i in range(len(nums)):
+            if nums[i] > numSum//k:
                 return False
-        visited = [False] * len(nums)
+        
+        visited = [False]*len(nums)
         return dfs(0,0,0,numSum//k)

fbf0e55f9a9addd141f789b907bab17036b823db
Rajat Kumar <54264843+rajat844@users.noreply.github.com>
2022-07-26 15:17:29 +0530

    Time: 52 ms (99.31%), Space: 13.9 MB (57.39%) - LeetHub

---
+++ 473-matchsticks-to-square/473-matchsticks-to-square.py
@@ -0,0 +1,36 @@
+class Solution:
+    def makesquare(self, matchsticks: List[int]) -> bool:
+        if len(matchsticks) < 4 or sum(matchsticks) % 4:
+            return False      
+        numSum = sum(matchsticks)
+        matchsticks.sort(reverse = True)
+        for x in matchsticks:
+            if x > numSum // 4:
+                return False
+        visited = [False] * len(matchsticks)
+        return self.dfs(matchsticks, 0, 0, 0, numSum // 4, visited)
+        
+    def dfs(self, a, made, cur, start, limit, visited):
+        if made == 4:
+            return True
+        if cur == limit:
+            return self.dfs(a, made + 1, 0, 0, limit, visited)
+        i = start
+        while i < len(a):
+            if visited[i] or a[i] + cur > limit:
+                i += 1
+                continue
+            visited[i] = True
+            if self.dfs(a, made, cur + a[i], i + 1, limit, visited):
+                return True
+            visited[i] = False
+            
+            while i + 1 < len(a) and a[i + 1] == a[i]: #pruning #1
+                i += 1
+            if not cur or cur + a[i] == limit: #pruning #2
+                return False
+            i += 1
+        return False
+            
+            
+        

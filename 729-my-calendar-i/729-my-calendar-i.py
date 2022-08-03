004a3575bd9018c4f3f9941c59085d00603d4d41
Rajat Kumar <54264843+rajat844@users.noreply.github.com>
2022-08-04 02:01:48 +0530

    Time: 532 ms (41.56%), Space: 15.1 MB (10.23%) - LeetHub

---
+++ 729-my-calendar-i/729-my-calendar-i.py
@@ -0,0 +1,18 @@
+from sortedcontainers import SortedList
+class MyCalendar:
+
+    def __init__(self):
+        self.st = []
+
+    def book(self, start: int, end: int) -> bool:
+        for i,j in self.st:
+            if i < end and j > start:
+                return False
+        self.st.append((start,end))
+        return True
+        
+
+
+# Your MyCalendar object will be instantiated and called as such:
+# obj = MyCalendar()
+# param_1 = obj.book(start,end)

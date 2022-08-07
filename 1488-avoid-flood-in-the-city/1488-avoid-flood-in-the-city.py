e9663f8a96095ab0ce3718a92ab5c5243f12b80f
Rajat Kumar <54264843+rajat844@users.noreply.github.com>
2022-08-08 02:45:49 +0530

    Time: 2488 ms (5.34%), Space: 30.3 MB (96.00%) - LeetHub

--- 1488-avoid-flood-in-the-city/1488-avoid-flood-in-the-city.py
+++ 1488-avoid-flood-in-the-city/1488-avoid-flood-in-the-city.py
@@ -1,27 +1,26 @@
 from sortedcontainers import SortedList
 class Solution:
     def avoidFlood(self, rains: List[int]) -> List[int]:
-        
         mp = {}
         st = SortedList()
-        ans = [0 for i in range(len(rains))]
-
+        ans = [1 for i in range(len(rains))]
+        
         for i in range(len(rains)):
             if rains[i] == 0:
                 st.add(i)
-            elif rains[i] > 0:
+            else:
                 if rains[i] in mp:
-                    a = bisect.bisect_right(st,mp[rains[i]])
-                    if a < len(st):
-                        ans[st[a]] = rains[i]
-                        st.remove(st[a])
+                    x = bisect.bisect_right(st,mp[rains[i]])
+                    if x < len(st):
+                        ans[st[x]] = rains[i]
+                        st.remove(st[x])
                     else:
                         return []
-                ans[i] = -1
                 mp[rains[i]] = i
-        
-        for i in range(len(ans)):
-            if ans[i] == 0:
-                ans[i] = 1
+                ans[i] = -1
         
         return ans
+                
+                    
+        
+        

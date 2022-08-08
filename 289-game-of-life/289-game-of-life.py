1a436fee954f738599b4579f919258804991d6bb
Rajat Kumar <54264843+rajat844@users.noreply.github.com>
2022-08-08 21:58:03 +0530

    Time: 43 ms (74.82%), Space: 14.1 MB (10.52%) - LeetHub

--- 289-game-of-life/289-game-of-life.py
+++ 289-game-of-life/289-game-of-life.py
@@ -2,37 +2,42 @@ class Solution:
     def gameOfLife(self, board: List[List[int]]) -> None:
         """
         Do not return anything, modify board in-place instead.
+        0 0 0
+        1 0 1
+        0 1 2
+        1 1 3
         """
-        # 0 0 0
-        # 1 0 1
-        # 0 1 2
-        # 1 1 3
-        def countindex(row,col):
-            cnt = 0
-            
+        def neighbours(row,col):
+            count = 0
             for i in range(row-1,row+2):
                 for j in range(col-1,col+2):
-                    if (i == row and j == col) or i < 0 or j < 0 or i >= n or j >= m:
+                    if i >= n or j >= m or i < 0 or j < 0 or (i==row and j ==col):
                         continue
                     elif board[i][j] == 1 or board[i][j] == 3:
-                        cnt += 1
-            return cnt
-        
-        
+                        count += 1
+            return count
+                    
+            
         n = len(board)
         m = len(board[0])
         
         for i in range(n):
             for j in range(m):
-                cnt = countindex(i,j)
                 
-                if board[i][j] == 0:
-                    if cnt == 3:
-                        board[i][j] = 2
-                        
-                elif cnt == 2 or cnt == 3:
-                    board[i][j] = 3
+                cnt = neighbours(i,j)
+                
+                if board[i][j] == 1:
+                    if cnt == 2 or cnt == 3:
+                        board[i][j] = 3
+                    
+                elif cnt == 3:
+                    board[i][j] = 2
         
         for i in range(n):
             for j in range(m):
-                board[i][j] = board[i][j] // 2
+                board[i][j] //=  2
+                    
+                    
+       
+        
+        

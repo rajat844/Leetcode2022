2693f9c5cbc79f0034e2489649193eafa43fa34c
Rajat Kumar <54264843+rajat844@users.noreply.github.com>
2022-08-08 22:36:04 +0530

    Time: 69 ms (74.04%), Space: 14.1 MB (27.74%) - LeetHub

--- 2327-number-of-people-aware-of-a-secret/2327-number-of-people-aware-of-a-secret.py
+++ 2327-number-of-people-aware-of-a-secret/2327-number-of-people-aware-of-a-secret.py
@@ -2,10 +2,12 @@ class Solution:
     def peopleAwareOfSecret(self, n: int, delay: int, forget: int) -> int:
         dp = [0 for i in range(n)]
         dp[0] = 1
+        prev = 0
         for i in range(1,n):
-            for j in range(i-forget+1, i-delay+1):
-                if i < 0:
-                    continue
-                dp[i] += dp[j]
+            fr_ppl = 0 if i - forget < 0 or dp[i-forget] < 0 else dp[i-forget]
+            dl_ppl = 0 if i - delay < 0 or dp[i-delay] < 0 else dp[i-delay]
+            dp[i] = prev + dl_ppl - fr_ppl
+            prev = dp[i]
+            
         
         return sum(dp[-forget:]) % (10**9+7)  

98b375585d426dc7a74e62bca46f65716d17eefc
Rajat Kumar <54264843+rajat844@users.noreply.github.com>
2022-08-10 21:23:14 +0530

    Time: 54 ms (44.94%), Space: 13.9 MB (58.85%) - LeetHub

---
+++ 263-ugly-number/263-ugly-number.py
@@ -0,0 +1,9 @@
+class Solution:
+    def isUgly(self, n: int) -> bool:
+        if n == 0:
+            return False
+        for num in (2,3,5):
+            while n % num == 0:
+                n = n // num
+                
+        return n == 1

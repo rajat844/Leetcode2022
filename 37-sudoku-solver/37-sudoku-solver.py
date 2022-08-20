7c333766ae53184a83a9afd1b4abac6d9cdb4a32
Rajat Kumar <54264843+rajat844@users.noreply.github.com>
2022-08-21 00:18:29 +0530

    Time: 1519 ms (6.40%), Space: 13.9 MB (96.35%) - LeetHub

--- 37-sudoku-solver/37-sudoku-solver.py
+++ 37-sudoku-solver/37-sudoku-solver.py
@@ -3,36 +3,32 @@ class Solution:
         """
         Do not return anything, modify board in-place instead.
         """
-   
-        def isValid(row,col,x):
+        def isValid(r,c,x):
             for i in range(9):
-                if board[i][col] == str(x):
+                if board[i][c] == str(x):
                     return False
-                if board[row][i] == str(x):
+                if board[r][i] == str(x):
                     return False
-                if board[3*(row//3)+ i//3][3*(col//3) + i%3] == str(x):
+                if board[3*(r//3) + i//3][3*(c//3) + i%3] == str(x):
                     return False
-                
-            return True
-        
-        def isValidSudoku():
-            for i in range(n):
-                for j in range(m):
-                    
-                    if board[i][j] == ".":
-                        for x in range(1,10):
-                            if isValid(i,j,x):
-                                board[i][j] = str(x)
-                                
-                                if isValidSudoku() == True:
-                                    return True
-                                else:
-                                    board[i][j] = "."
-                        
-                        return False
-                    
+            
             return True
         
         n = len(board)
         m = len(board[0])
-        isValidSudoku()
+        def helper():
+            for i in range(n):
+                for j in range(m):
+                    if board[i][j] == ".":
+                        for x in range(1,10):
+                            if isValid(i,j,x):
+                                board[i][j] = str(x)
+                                if helper() ==  True:
+                                    return True
+                                board[i][j] = "."
+                        return False
+            return True
+                
+        
+        return helper()
+                                

3d8fb93648be50ecc3287ba03629db9dfeb10ce8
Rajat Kumar <54264843+rajat844@users.noreply.github.com>
2022-08-23 13:46:50 +0530

    Time: 152 ms (86.82%), Space: 14.9 MB (16.79%) - LeetHub

---
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.py
@@ -0,0 +1,26 @@
+class Solution:
+    def minDeletions(self, s: str) -> int:
+        st = Counter(s)
+        arr = []
+        for x in st:
+            arr.append(st[x])
+        
+        arr.sort(reverse = True)
+        
+        print(arr)
+        mx = math.inf
+        ans = 0
+        for i in range(len(arr)):
+            if mx == 0:
+                ans += arr[i]
+            elif arr[i] > mx:
+                ans += arr[i] - mx
+                mx -= 1
+            else :
+                mx = arr[i] - 1
+                
+        return ans
+                
+            
+        
+            

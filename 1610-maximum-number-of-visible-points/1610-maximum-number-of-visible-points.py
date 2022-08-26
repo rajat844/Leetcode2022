2017d87688dd3dce28f5f52e4c386d2a79be757c
Rajat Kumar <54264843+rajat844@users.noreply.github.com>
2022-08-26 18:17:08 +0530

    Time: 3690 ms (52.21%), Space: 47.1 MB (93.29%) - LeetHub

--- 1610-maximum-number-of-visible-points/1610-maximum-number-of-visible-points.py
+++ 1610-maximum-number-of-visible-points/1610-maximum-number-of-visible-points.py
@@ -1,36 +1,38 @@
 class Solution:
     def visiblePoints(self, points: List[List[int]], angle: int, location: List[int]) -> int:
         def getAngle(p):
-            angle = math.atan2(p[0]-location[0],p[1]-location[1])/(2*math.pi) * 360
-            if angle < 0:
-                angle += 360
+            a = math.atan2(p[0]-location[0],p[1]-location[1])/(2*math.pi) * 360
+            if a < 0:
+                a += 360
             
-            return angle
+            return a
         
-        angles = []
         same = 0
-        
-        for i in range(len(points)):
-            if points[i] == location:
+        arr = []
+        for p in points:
+            if p == location:
                 same += 1
             else:
-                angles.append(getAngle(points[i]))
-
-        angles.sort()
+                arr.append(getAngle(p))
+        
+        arr.sort()
         st = deque()
         ans = 0
         
-        for i in range(len(angles)):
-            st.append(angles[i])
-            while angles[i] - st[0] > angle:
+        for ele in arr:
+            st.append(ele)
+            while ele - st[0] > angle:
                 st.popleft()
             ans = max(ans,len(st))
-        
-        for i in range(len(angles)):
-            angles[i] += 360
-            st.append(angles[i])
-            while angles[i] - st[0] > angle:
+            
+        for ele in arr:
+            ele += 360
+            st.append(ele)
+            while ele - st[0] > angle:
                 st.popleft()
             ans = max(ans,len(st))
-        
+            
         return ans+same
+        
+                
+                

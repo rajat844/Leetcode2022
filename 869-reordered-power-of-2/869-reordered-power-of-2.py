52d400c8949c0645e1f8a87f66be5a60b534d622
Rajat Kumar <54264843+rajat844@users.noreply.github.com>
2022-08-26 13:48:41 +0530

    Time: 37 ms (89.84%), Space: 13.8 MB (96.79%) - LeetHub

--- 869-reordered-power-of-2/869-reordered-power-of-2.py
+++ 869-reordered-power-of-2/869-reordered-power-of-2.py
@@ -1,7 +1,7 @@
 class Solution:
     def reorderedPowerOf2(self, n: int) -> bool:
         def countDigits(num):
-            arr = [0 for u in range(10)]
+            arr = [0 for i in range(10)]
             while num:
                 arr[num%10] += 1
                 num = num//10
@@ -10,10 +10,12 @@ class Solution:
         
         cnt = countDigits(n)
         y = 1
+        
         for i in range(31):
             if cnt == countDigits(y):
                 return True
             y = y << 1
         
         return False
+            
         

c685b3093cc2dc678e87a78906fd96bafacb8756
Rajat Kumar <54264843+rajat844@users.noreply.github.com>
2022-09-01 12:59:11 +0530

    Time: 5547 ms (94.66%), Space: 17 MB (71.01%) - LeetHub

--- 312-burst-balloons/312-burst-balloons.py
+++ 312-burst-balloons/312-burst-balloons.py
@@ -1,23 +1,27 @@
 class Solution:
-    def maxCoins(self, nums: List[int]) -> int:        
+    def maxCoins(self, nums: List[int]) -> int:
+        def burst(i,j):
+            if i > j:
+                return 0
+            ans = -math.inf
+            for x in range(i,j+1):
+                ans = max(ans,nums[i-1]*nums[x]*nums[j+1] + burst(i,x-1)+burst(x+1,j))
+                
+            return ans
+        
         n = len(nums)
-        nums = [1] + nums + [1]
-        dp = [[0 for i in range(n+2)] for j in range(n+2)]
+        nums = [1]+nums+[1]
+        dp = [[0 for i in range(n+2)]for j in range(n+2)]
         
         for i in range(n,0,-1):
             for j in range(1,n+1):
-                if i > j:
+                if i > j :
                     continue
                 ans = -math.inf
                 for x in range(i,j+1):
-                    cost = nums[i-1]*nums[x]*nums[j+1] +  dp[i][x-1] + dp[x+1][j]
-                    ans = max(cost,ans)
+                    ans = max(ans,nums[i-1]*nums[x]*nums[j+1] + dp[i][x-1]+dp[x+1][j])
                     
-                dp[i][j] = ans               
-                
+                dp[i][j] = ans
+        
         return dp[1][n]
-    
-                
-                
-            
         

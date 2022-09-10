fb06b70fabc4012a1a34fac34632362dcf9d1cdb
Rajat Kumar <54264843+rajat844@users.noreply.github.com>
2022-09-11 02:59:33 +0530

    Time: 529 ms (6.15%), Space: 17.1 MB (51.90%) - LeetHub

--- 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.py
+++ 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.py
@@ -1,15 +1,26 @@
 class Solution:
-    def maxProfit(self, k: int, prices: List[int]) -> int:
+    def maxProfit(self, cnt: int, prices: List[int]) -> int:
         n = len(prices)
-        dp = [[[0 for i in range(k+1)]for i in range(2)]for i in range(n+1)]
         
+        dp = [[[0 for i in range(cnt+1)]for j in range(2)]for k in range(n+1)]
+    
         for i in range(n-1,-1,-1):
             for j in range(2):
-                for k in range(1,k+1):
-                    
+                for k in range(cnt-1,-1,-1):
                     if j == 1:
-                        dp[i][j][k] = max(-prices[i]+dp[i+1][0][k],dp[i+1][1][k])
+                        dp[i][j][k] =  max(prices[i]+dp[i+1][0][k+1],dp[i+1][1][k])
                     else:
-                        dp[i][j][k] = max(prices[i]+dp[i+1][1][k-1],dp[i+1][0][k])
+                        dp[i][j][k] =  max(-prices[i]+dp[i+1][1][k],dp[i+1][0][k]) 
+            
+        return dp[0][0][0]
         
-        return dp[0][1][k]
+#         def helper(i,isSell,cnt):
+#             if i == len(prices) or cnt == k:
+#                 return 0
+            
+#             if isSell == 1:
+#                 return max(prices[i]+helper(i+1,0,cnt+1) , helper(i+1,1,cnt))
+#             else:
+#                 return max(-prices[i]+helper(i+1,1,cnt),helper(i+1,0,cnt))
+        
+#         return helper(0,0,0)

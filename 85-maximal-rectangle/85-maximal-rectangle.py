102d1ed2cf3113eed9fb229b690d9bdda86d72d6
Rajat Kumar <54264843+rajat844@users.noreply.github.com>
2022-09-20 14:58:51 +0530

    Time: 297 ms (87.90%), Space: 15.3 MB (56.47%) - LeetHub

--- 85-maximal-rectangle/85-maximal-rectangle.py
+++ 85-maximal-rectangle/85-maximal-rectangle.py
@@ -1,10 +1,10 @@
 class Solution:
     def maximalRectangle(self, matrix: List[List[str]]) -> int:
-        def maxAreaHistogram(arr):
+        def maxArea(arr):
             st = []
-            res = 0
+            area = 0
             
-            for i in range(len(arr)+1):
+            for i in range(len(arr) + 1):
                 while len(st) > 0 and (i == len(arr) or arr[i] <= arr[st[-1]]):
                     h = arr[st.pop()]
                     w = 0
@@ -12,30 +12,25 @@ class Solution:
                         w = i
                     else:
                         w = i - st[-1] - 1
-                    
-                    res = max(res,h*w)
+                    area = max(area,h*w)
                 st.append(i)
-            return res
+            
+            return area
         
-        n = len(matrix)
-        m = len(matrix[0])
-        temp = [0 for i in range(m)]
+        temp = [0 for i in range(len(matrix[0]))]
         ans = 0
-    
-        for i in range(n):
-            for j in range(m):
+        for i in range(len(matrix)):
+            for j in range(len(matrix[0])):
                 if matrix[i][j] == "0":
                     temp[j] = 0
                 else:
                     temp[j] += 1
-                    
-            ans = max(ans,maxAreaHistogram(temp))
-        
+            
+            ans = max(ans,maxArea(temp))
+            
         return ans
             
-        
-        
+            
                     
-                                       
                     
         

4aca16878c2515655daa8a4d64f75309d56cc000
Rajat Kumar <54264843+rajat844@users.noreply.github.com>
2023-02-13 11:09:41 +0530

    Time: 103 ms (45.93%), Space: 97.1 MB (72.31%) - LeetHub

---
+++ 2477-minimum-fuel-cost-to-report-to-the-capital/2477-minimum-fuel-cost-to-report-to-the-capital.java
@@ -0,0 +1,29 @@
+class Solution {
+    long cars = 0;
+    public long minimumFuelCost(int[][] roads, int seats) {
+        HashMap<Integer,ArrayList<Integer>> st = new HashMap<Integer,ArrayList<Integer>>();
+        
+        for(int i = 0; i<roads.length; i++){
+            st.putIfAbsent(roads[i][0],new ArrayList<Integer>());
+            st.get(roads[i][0]).add(roads[i][1]);
+            st.putIfAbsent(roads[i][1],new ArrayList<Integer>());
+            st.get(roads[i][1]).add(roads[i][0]);
+        }
+        
+        long pass = travel(st,seats,0,-1);
+        return cars;
+    }
+    
+    public long travel(HashMap<Integer,ArrayList<Integer>>st, int seats, int node, int parent){
+        long pas = 1;
+        ArrayList<Integer> childs = st.get(node);
+        if(childs != null){
+        for(Integer x :childs){
+            if(x != parent){
+                pas += travel(st,seats,x,node);
+            }
+        }}
+        if(node != 0) cars += (pas + seats - 1)/seats;
+        return pas;
+    }
+}

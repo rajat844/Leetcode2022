dd76abc43b5528d169d138947e68c8dddc137150
Rajat Kumar <54264843+rajat844@users.noreply.github.com>
2024-06-11 09:02:49 +0530

    Time: 2 ms (80.16%), Space: 7 MB (72.02%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -2,13 +2,14 @@ class Solution {
 public:
     int fib(int n) {
         if (n < 2) return n;
-        int arr[n + 1];
-        arr[0] = 0;
-        arr[1] = 1;
+        int prev1 = 0;
+        int curr = 1;
         for(int i = 2; i<= n; i++){
-            arr[i] = arr[i-1] + arr[i-2];
+            int temp = prev1 + curr;
+            prev1 = curr;
+            curr = temp;
         }
         
-        return arr[n];
+        return curr;
     }
 };

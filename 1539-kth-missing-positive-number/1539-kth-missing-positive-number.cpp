30dd1399a7a605316d52998da803f209cc00fea4
Rajat Kumar <54264843+rajat844@users.noreply.github.com>
2024-07-25 00:00:49 +0530

    Time: 4 ms (40.07%), Space: 12.4 MB (16.79%) - LeetHub

--- 1539-kth-missing-positive-number/1539-kth-missing-positive-number.cpp
+++ 1539-kth-missing-positive-number/1539-kth-missing-positive-number.cpp
@@ -4,13 +4,14 @@ public:
         int left = 0;
         int right = arr.size() - 1;
         
-        while(left <= right){
+        while (left <= right){
             int mid = (left + right) >> 1;
             
-            if((arr[mid] - mid - 1) < k) left = mid + 1;
+            if (arr[mid] - mid - 1 < k) left = mid + 1;
             else right = mid - 1;
         }
         
         return left + k;
+        
     }
 };

aeca852379f0d1d291193947bc32d0fbf71c015c
Rajat Kumar <54264843+rajat844@users.noreply.github.com>
2024-08-17 10:28:53 +0530

    Time: 267 ms (22.13%), Space: 30.4 MB (9.28%) - LeetHub

--- 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.cpp
+++ 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.cpp
@@ -3,22 +3,22 @@ public:
     int beautySum(string s) {
         int ans = 0;
         
+        
         for(int i = 0; i < s.size(); i++){
             map<char,int> st;
-
             
-            for(int j = i; j < s.length(); j++){
+            for(int j = i; j < s.size(); j++){
                 st[s[j]]++;
                 
                 int mx = INT_MIN;
                 int mn = INT_MAX;
                 
-                for(auto const& [key,val]: st){
+                for(auto const& [key,val] : st){
                     mx = max(val,mx);
                     mn = min(val,mn);
-                } 
+                }
                 
-                ans += mx - mn;
+                ans += mx-mn;
             }
         }
         

ce38313574ec73aa1d557784e0e51f8b85a28f32
Rajat Kumar <54264843+rajat844@users.noreply.github.com>
2024-10-02 12:17:39 +0530

    Time: 8 ms (82.52%), Space: 10.4 MB (57.99%) - LeetHub

--- 0402-remove-k-digits/0402-remove-k-digits.cpp
+++ 0402-remove-k-digits/0402-remove-k-digits.cpp
@@ -4,30 +4,29 @@ public:
         vector<char> st;
         
         for(char x : num){
-            while(!st.empty() && st.back() > x && k > 0){
-                k--;
+            while(st.size() > 0 && st.back() > x && k > 0){
                 st.pop_back();
+                k--;
             }
             
             if(st.size() == 0 && x == '0')
                 continue;
-            else 
-                st.push_back(x);
+            st.push_back(x);
         }
-        while(k > 0 && st.size() > 0){
-            st.pop_back();
-            k --;
-        }
-        string ans = "";
         
-        for(int i = 0; i < st.size(); i++){
-            if(i == 0 && st[i] == '0') continue;
-            ans += st[i];
+        while(st.size() > 0 && k > 0){
+            st.pop_back();
+            k--;
+        }
+        
+        string ans;
+        
+        for(char x : st){
+            ans += x;
         }
         
         if(ans.size() == 0) return "0";
         return ans;
-        
         
     }
 };

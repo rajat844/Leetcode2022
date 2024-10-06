cc63abddb77c1d22c4a8cd819da9e4441b609c8a
Rajat Kumar <54264843+rajat844@users.noreply.github.com>
2024-10-06 22:42:07 +0530

    Time: 10 ms (55.00%), Space: 20.3 MB (92.83%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.cpp
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -3,15 +3,20 @@ public:
     vector<int> asteroidCollision(vector<int>& asteroids) {
         vector<int> ans;
         
-        for(int x : asteroids){
-            if(x > 0) ans.push_back(x);
+        for(int i = 0; i < asteroids.size(); i++){
+            if(asteroids[i] > 0){
+                ans.push_back(asteroids[i]);
+            }
+            
             else{
-                while(ans.size() > 0 && ans.back() > 0 && ans.back() < abs(x))ans.pop_back();
-                if(ans.size() > 0 && ans.back() == abs(x)) ans.pop_back();
-                else if(ans.size() == 0 || ans.back() < 0) ans.push_back(x);
+                while(ans.size() > 0 && ans.back() > 0 && ans.back() < abs(asteroids[i])) ans.pop_back();
+                
+                if(ans.size() > 0 && ans.back() > 0 && ans.back() == abs(asteroids[i])) ans.pop_back();
+                else if(ans.size() == 0 || ans.back() < 0){
+                    ans.push_back(asteroids[i]);
+                }
             }
         }
-        
         return ans;
         
     }

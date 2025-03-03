44a1f0c8049de43935e3499912e747f92a80f50c
Rajat Kumar <54264843+rajat844@users.noreply.github.com>
2025-03-04 04:47:56 +0530

    Time: 0 ms (100%), Space: 13.2 MB (87.17%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.cpp
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -3,13 +3,23 @@ public:
     int singleNumber(vector<int>& nums) {
         int ones = 0;
         int twos = 0;
-        
-        for(int x : nums){
-            ones = (ones ^ x) & (~twos);
-            twos = (twos ^ x) & (~ones);
+
+        for(int i =0; i < nums.size(); i++){
+            ones = (ones ^ nums[i]) & (~twos);
+            twos = (twos ^ nums[i]) & (~ones);
         }
-        
+
         return ones;
-        
     }
 };
+
+//  first occ = ! two
+// second occ =  present in ones
+
+
+// first -> add it to ones. 
+// second -> add it to two, remove it from ones
+// third -> remove it from two
+
+
+// o -> ^ 

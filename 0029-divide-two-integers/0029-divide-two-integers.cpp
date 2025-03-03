dd4595eea696a40fb6e3dc9e8ac74a357c768e41
Rajat Kumar <54264843+rajat844@users.noreply.github.com>
2025-03-04 03:19:56 +0530

    Time: 0 ms (100%), Space: 8.6 MB (38.19%) - LeetHub

--- 0029-divide-two-integers/0029-divide-two-integers.cpp
+++ 0029-divide-two-integers/0029-divide-two-integers.cpp
@@ -1,28 +1,32 @@
 class Solution {
 public:
     int divide(int dividend, int divisor) {
-        if(dividend == divisor) return 1;
-        
+        if (dividend == divisor)
+            return 1;
         bool sign = true;
-        
-        if((dividend >= 0 && divisor < 0) || (dividend < 0 && divisor >= 0)) sign = false;
-        
-        long n = abs(dividend);
-        long d = abs(divisor);
+
+        if (dividend >= 0 && divisor < 0 || dividend < 0 && divisor >= 0)
+            sign = false;
+
+        long n = abs((long)dividend);
+        long d = abs((long)divisor);
+
         long ans = 0;
-        
-        while(n >= d){
-            long count = 0;
-            
-            while(n >= (d << (count + 1))) count += 1;
-            
+
+        while (n >= d) {
+            int count = 0;
+            while (n >= (d << count + 1))
+                count += 1;
+
+            ans += (1 << count);
             n -= (d << count);
-            ans += 1 << count;
         }
-        
-        if(ans == (1 << 31) && sign) return INT_MAX;
-        if(ans == (1 << 31) && !sign) return INT_MIN;
-        return (sign) ? ans : ans * -1;
-                
+
+        if (ans == (1 << 31) && sign)
+            return INT_MAX;
+        if (ans == (1 << 31) && !sign)
+            return INT_MIN;
+
+        return (sign == true) ? ans : -ans;
     }
 };

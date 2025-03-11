463bcc51404e5411094de914407856fa0ad9d9b1
Rajat Kumar <54264843+rajat844@users.noreply.github.com>
2025-03-12 03:41:18 +0530

    Time: 2 ms (92.5%), Space: 11.2 MB (35.02%) - LeetHub

--- 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.cpp
+++ 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.cpp
@@ -6,24 +6,17 @@ public:
         int count = 0;
         int ans = 0;
         int left = 0;
-     
 
         for (int right = 0; right < s.size(); right++) {
-            cout << ans << endl;
             arr[s[right] - 'a']++;
             if (arr[s[right] - 'a'] == 1)
                 count += 1;
 
-            if (count == 3)
-                ans += n - right;
-
             while (count == 3) {
+                ans += n - right;
                 arr[s[left] - 'a']--;
                 if (arr[s[left] - 'a'] == 0)
                     count -= 1;
-                else {
-                    ans += n - right;
-                }
                 left += 1;
             }
         }
